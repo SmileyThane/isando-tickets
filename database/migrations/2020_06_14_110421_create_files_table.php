3ea5553637e8a242979a97c99b4408b912381c18
SmileyThane <thane.crios@gmail.com>
2020-06-14 18:25:23 +0300

    added file uploading on system and for ticket adding method

---
+++ database/migrations/2020_06_14_110421_create_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 256);
+            $table->text('filepath');
+            $table->bigInteger('model_id');
+            $table->string('model_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
