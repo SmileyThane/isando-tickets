fef7ed9fb168a21ebb73328143c450f3ff6e3def
SmileyThane <thane.crios@gmail.com>
2020-06-30 14:13:08 +0300

    created console job and modified api call for apply

---
+++ app/Jobs/ReceiveSystemMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
+
+class ReceiveSystemMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $request = Request::create(route('receiveEmail'), 'GET');
+        app()->handle($request);
+        Log::info('email cache was checked - ' . now());
+    }
+}
