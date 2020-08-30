18eb31a5be06c25a4817c122ffe82b30d029d78a
SmileyThane <thane.crios@gmail.com>
2020-08-30 16:48:29 +0300

    created new seeder for types

---
+++ database/seeds/TypesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('address_types')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name' => 'Home'
+            ]
+        );
+        DB::table('phone_types')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name' => 'Mobile'
+            ]
+        );
+        DB::table('social_types')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name' => 'Website'
+            ]
+        );
+    }
+}
