416184aeba3ae80ba945258a2694eee1511fa8df
SmileyThane <thane.crios@gmail.com>
2020-09-04 22:53:40 +0300

    fixed lang seeder anfter last update

--- database/seeds/LanguageSeeder.php
+++ database/seeds/LanguageSeeder.php
@@ -129,7 +129,7 @@ class LanguageSeeder extends Seeder
 "profile":"profile",
 "availability_description": "Please provide working hours when we can contact you on the submitted issue",
 "ip_description": "IP addresses of your server for remote access if applicable",
-"access_desctiption": "Please provide login details to your server for Teamviewer access, if applicable",
+"access_description": "Please provide login details to your server for Teamviewer access, if applicable",
 "answer_description": "Write you answer here..."
 
    },
@@ -257,7 +257,7 @@ class LanguageSeeder extends Seeder
 "profile":"profil",
 "availability_description": "Bitte teilen Sie uns mit, zu welcher Zeit Sie verfügbar sind, damit wir Sie umgehend kontaktieren können.",
 "ip_description": " IP-Adressen Ihres Servers für den Fernzugriff, falls vorhanden.",
-"access_desctiption": "Bitte geben Sie uns Ihre Teamviewer Zugangsdaten zu Ihrem Server an, falls vorhanden.",
+"access_description": "Bitte geben Sie uns Ihre Teamviewer Zugangsdaten zu Ihrem Server an, falls vorhanden.",
 "answer_description": "Schreiben Sie Ihre Antwort hier..."
 
 
