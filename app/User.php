5661cbe4719dc63487ea0a7bd44dcae8e16dcef4
SmileyThane <thane.crios@gmail.com>
2020-09-04 20:34:13 +0300

    modified user model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email','surname','title_before_name','title','country', 'anredeform', 'language_id', 'timezone_id'
+        'name', 'email','surname','title_before_name','title','country_id', 'anredeform', 'language_id', 'timezone_id'
     ];
 
     /**
