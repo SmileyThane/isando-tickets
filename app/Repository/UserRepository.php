4195f54d1d21ba4b879709803d03cbc2976af35f
smileythane <thane.crios@gmail.com>
2020-09-05 17:06:42 +0300

    updated build. removed empty line from user repo. updated composer.lock

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -36,7 +36,6 @@ class UserRepository
 
     public function find($id, $with = [])
     {
-
         return User::where('id', $id)->with(array_merge(['phones.type', 'addresses.type'], $with))->first();
     }
 
