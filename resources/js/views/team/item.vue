a9d68745817166f8479ee96a111f55ca0dfd711b
smileythane <thane.crios@gmail.com>
2020-09-05 16:35:40 +0300

    updated single team view. added translations. optimized components

--- resources/js/views/team/item.vue
+++ resources/js/views/team/item.vue
@@ -17,17 +17,19 @@
                         dark
                         flat
                     >
-                        <v-toolbar-title>Basic info</v-toolbar-title>
+                        <v-toolbar-title>{{ langMap.main.info }}</v-toolbar-title>
                         <v-spacer></v-spacer>
                         <v-icon v-if="!enableToEdit" @click="enableToEdit = true">mdi-pencil</v-icon>
-                        <v-btn v-if="enableToEdit" color="white" style="color: black;" @click="updateTeam">Save</v-btn>
+                        <v-btn v-if="enableToEdit" color="white" style="color: black;" @click="updateTeam">
+                            {{ langMap.main.update }}
+                        </v-btn>
 
                     </v-toolbar>
                     <v-card-text>
                         <v-form>
                             <v-text-field
                                 color="green"
-                                label="Name"
+                                :label="langMap.main.name"
                                 name="team_name"
                                 prepend-icon="mdi-rename-box"
                                 type="text"
@@ -38,7 +40,7 @@
                             ></v-text-field>
                             <v-text-field
                                 color="green"
-                                label="Description"
+                                :label="langMap.main.description"
                                 name="team_description"
                                 prepend-icon="mdi-comment-text"
                                 type="text"
@@ -49,10 +51,6 @@
                             ></v-text-field>
                         </v-form>
                     </v-card-text>
-                    <!--                    <v-card-actions>-->
-                    <!--                        <v-spacer></v-spacer>-->
-                    <!--                        <v-btn color="green" style="color: white;" @click="updateTeam">Save</v-btn>-->
-                    <!--                    </v-card-actions>-->
                 </v-card>
             </div>
             <div class="col-md-6">
@@ -63,15 +61,15 @@
                         dark
                         flat
                     >
-                        <v-toolbar-title>Team members</v-toolbar-title>
+                        <v-toolbar-title>{{ langMap.main.team }} {{ langMap.main.members }}</v-toolbar-title>
                         <v-spacer></v-spacer>
                     </v-toolbar>
                     <v-card-text>
                         <v-data-table
                             :headers="headers"
                             :items="team.employees"
-                            :items-per-page="25"
                             class="elevation-1"
+                            :footer-props="footerProps"
                         ></v-data-table>
                         <v-spacer>
                             &nbsp;
@@ -79,7 +77,7 @@
                         <v-expansion-panels>
                             <v-expansion-panel>
                                 <v-expansion-panel-header>
-                                    New Team Member
+                                    {{ langMap.main.new }} {{ langMap.main.team }} {{ langMap.main.member }}
                                     <template v-slot:actions>
                                         <v-icon color="submit">mdi-plus</v-icon>
                                     </template>
@@ -95,7 +93,7 @@
                                                     item-value="id"
                                                     v-model="employeeForm.company_user_id"
                                                     :items="companies.employees"
-                                                    label="To"
+                                                    :label="langMap.main.email"
                                                 ></v-autocomplete>
                                             </v-col>
                                             <v-btn
@@ -124,124 +122,130 @@
 
 
 <script>
-    export default {
+export default {
 
-        data() {
-            return {
-                snackbar: false,
-                actionColor: '',
-                snackbarMessage: '',
-                enableToEdit: false,
-                headers: [
+    data() {
+        return {
+            snackbar: false,
+            actionColor: '',
+            snackbarMessage: '',
+            enableToEdit: false,
+            langMap: this.$store.state.lang.lang_map,
+            footerProps: {
+                itemsPerPage: 10,
+                disableItemsPerPage: true,
+                itemsPerPageText: this.$store.state.lang.lang_map.main.items_per_page
+            },
+            headers: [
+                {
+                    text: 'ID',
+                    align: 'start',
+                    sortable: false,
+                    value: 'employee.id',
+                },
+                {text: `${this.$store.state.lang.lang_map.main.name}`, value: 'employee.user_data.name'},
+                {text: `${this.$store.state.lang.lang_map.main.email}`, value: 'employee.user_data.email'},
+                {text: `${this.$store.state.lang.lang_map.main.roles}`, value: 'employee.role_names'},
+                {text: `${this.$store.state.lang.lang_map.main.actions}`, value: ''},
+            ],
+            team: {
+                team_name: '',
+                team_description: '',
+                team_owner_id: '',
+                employees: [
                     {
-                        text: 'ID',
-                        align: 'start',
-                        sortable: false,
-                        value: 'employee.id',
-                    },
-                    {text: 'name', value: 'employee.user_data.name'},
-                    {text: 'email', value: 'employee.user_data.email'},
-                    {text: 'roles', value: 'employee.role_names'},
-                    {text: 'Actions', value: ''},
-                ],
-                team: {
-                    team_name: '',
-                    team_description: '',
-                    team_owner_id: '',
-                    employees: [
-                        {
-                            employee: {
-                                user_id: '',
-                                company_id: '',
-                                roles: [],
-                                user_data: ''
+                        employee: {
+                            user_id: '',
+                            company_id: '',
+                            roles: [],
+                            user_data: ''
 
-                            }
                         }
-                    ]
-                },
-                employeeForm: {
-                    company_user_id: '',
-                    team_id: ''
-                },
-                companies: [
-                    {
-                        id: '',
-                        name: '',
-                        employees: [
-                            {
-                                user_data:
-                                    {
-                                        email: '',
-                                        name: ''
-                                    }
-                            }
-                        ]
                     }
                 ]
+            },
+            employeeForm: {
+                company_user_id: '',
+                team_id: ''
+            },
+            companies: [
+                {
+                    id: '',
+                    name: '',
+                    employees: [
+                        {
+                            user_data:
+                                {
+                                    email: '',
+                                    name: ''
+                                }
+                        }
+                    ]
+                }
+            ]
 
-            }
+        }
+    },
+    mounted() {
+        this.getTeam()
+        this.employeeForm.team_id = this.$route.params.id
+    },
+    methods: {
+        getTeam() {
+            axios.get(`/api/team/${this.$route.params.id}`).then(response => {
+                response = response.data
+                if (response.success === true) {
+                    this.team = response.data
+                    this.team.team_name = response.data.name
+                    this.team.team_description = response.data.description
+                    this.getCompanies()
+                } else {
+                    console.log('error')
+                }
+
+            });
         },
-        mounted() {
-            this.getTeam()
-            this.employeeForm.team_id = this.$route.params.id
+        getCompanies() {
+            console.log(this.team);
+            axios.get(`/api/company/${this.team.team_owner_id}`).then(response => {
+                response = response.data
+                if (response.success === true) {
+                    this.companies = response.data
+                    console.log(this.companies);
+                } else {
+                    console.log('error')
+                }
+
+            });
         },
-        methods: {
-            getTeam() {
-                axios.get(`/api/team/${this.$route.params.id}`).then(response => {
-                    response = response.data
-                    if (response.success === true) {
-                        this.team = response.data
-                        this.team.team_name = response.data.name
-                        this.team.team_description = response.data.description
-                        this.getCompanies()
-                    } else {
-                        console.log('error')
-                    }
+        addEmployee() {
+            // console.log(this.employeeForm);
+            axios.post(`/api/team/employee`, this.employeeForm).then(response => {
+                response = response.data
+                if (response.success === true) {
+                    this.getTeam()
+                } else {
+                    console.log('error')
+                }
 
-                });
-            },
-            getCompanies() {
-                console.log(this.team);
-                axios.get(`/api/company/${this.team.team_owner_id}`).then(response => {
-                    response = response.data
-                    if (response.success === true) {
-                        this.companies = response.data
-                        console.log(this.companies);
-                    } else {
-                        console.log('error')
-                    }
+            });
+        },
+        updateTeam() {
+            axios.patch(`/api/team/${this.$route.params.id}`, this.team).then(response => {
+                response = response.data
+                if (response.success === true) {
+                    this.team.team_name = response.data.name
+                    this.team.team_description = response.data.description
+                    this.enableToEdit = false
+                    this.snackbarMessage = 'Update successful'
+                    this.actionColor = 'success'
+                    this.snackbar = true;
+                } else {
+                    console.log('error')
+                }
 
-                });
-            },
-            addEmployee() {
-                // console.log(this.employeeForm);
-                axios.post(`/api/team/employee`, this.employeeForm).then(response => {
-                    response = response.data
-                    if (response.success === true) {
-                        this.getTeam()
-                    } else {
-                        console.log('error')
-                    }
-
-                });
-            },
-            updateTeam() {
-                axios.patch(`/api/team/${this.$route.params.id}`, this.team).then(response => {
-                    response = response.data
-                    if (response.success === true) {
-                        this.team.team_name = response.data.name
-                        this.team.team_description = response.data.description
-                        this.enableToEdit = false
-                        this.snackbarMessage = 'Update successful'
-                        this.actionColor = 'success'
-                        this.snackbar = true;
-                    } else {
-                        console.log('error')
-                    }
-
-                });
-            }
+            });
         }
     }
+}
 </script>
