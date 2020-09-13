f57904ae5a50681cb0569681d569b44dec4bd070
SmileyThane <thane.crios@gmail.com>
2020-09-13 18:39:57 +0300

    created address type seeder

---
+++ database/seeds/AddressTypeSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AddressTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('address_types')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name' => 'home',
+            ]
+        );
+        DB::table('address_types')->updateOrInsert(
+            ['id' => 2],
+            [
+                'name' => 'work',
+            ]
+        );
+        DB::table('address_types')->updateOrInsert(
+            ['id' => 3],
+            [
+                'name' => 'other',
+            ]
+        );
+    }
+}
