c49af5b953aeab8b1a0854dacfbd77ecb1d3fbd3
SmileyThane <thane.crios@gmail.com>
2020-09-15 19:18:25 +0300

    created and grouped new items on sidebar view. updated build

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -23,30 +23,45 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.home}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-            <v-list-item link to="/company">
-                <v-list-item-action>
-                    <v-icon>mdi-office-building</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link to="/customer" v-if="checkRoleByIds([1,2,3])">
-                <v-list-item-action>
-                    <v-icon>mdi-account-network</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.customers}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link to="/product">
-                <v-list-item-action>
-                    <v-icon> mdi-monitor-clean</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.products}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
+            <v-list-group
+                prepend-icon="mdi-account"
+                :value="sidebarGroups"
+                active-class="green--text"
+                color="green"
+                multiple
+            >
+                <template
+                    v-slot:activator
+                >
+                    <v-list-item-content>
+                        <v-list-item-title>{{customers}} - CRM</v-list-item-title>
+                    </v-list-item-content>
+                </template>
+                <v-list-item link to="/customer" v-if="checkRoleByIds([1,2,3])">
+                    <v-list-item-action>
+                        <v-icon>mdi-factory</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.customers}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to="/employee" v-if="checkRoleByIds([1,2,3])">
+                    <v-list-item-action>
+                        <v-icon>mdi-badge-account-horizontal-outline</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.individuals}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to="/product">
+                    <v-list-item-action>
+                        <v-icon> mdi-monitor-clean</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.products}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
             <v-list-item link to="/team" v-if="checkRoleByIds([1,2,3])">
                 <v-list-item-action>
                     <v-icon>mdi-account-box-multiple-outline</v-icon>
@@ -55,19 +70,12 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.teams}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-            <v-list-item link to="/employee" v-if="checkRoleByIds([1,2,3])">
-                <v-list-item-action>
-                    <v-icon>mdi-card-account-details-outline</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.individuals}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
             <v-list-group
                 prepend-icon="mdi-ticket-account"
-                value="true"
                 active-class="green--text"
+                :value="sidebarGroups"
                 color="green"
+                multiple
             >
                 <template
                     v-slot:activator
@@ -95,6 +103,63 @@
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
+                <v-list-item link to="/">
+                    <v-list-item-action>
+                        <v-icon>mdi-email-send-outline</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.notify_customers}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+            <v-list-item link to="/">
+                <v-list-item-action>
+                    <v-icon>mdi-book-open-page-variant</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.knowledge_base}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-group
+                prepend-icon="mdi-cog"
+                active-class="green--text"
+                :value="sidebarGroups"
+                color="green"
+                multiple
+                v-if="checkRoleByIds([1,2,3])"
+            >
+            <template
+                v-slot:activator
+            >
+                <v-list-item-content>
+
+                    <v-list-item-title>{{settings}}</v-list-item-title>
+                </v-list-item-content>
+            </template>
+            <v-list-item link to="/company" v-if="checkRoleByIds([1,2,3])">
+                <v-list-item-action>
+                    <v-icon>mdi-office-building</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
+                <v-list-item-action>
+                    <v-icon>mdi-folder-cog-outline</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.system_settings}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
+                <v-list-item-action>
+                    <v-icon>mdi-cogs</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.general_settings}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
             </v-list-group>
         </v-list>
     </v-navigation-drawer>
@@ -108,7 +173,11 @@
             companyName: 'Isando',
             localDrawer: null,
             show: true,
-            ticket: ''
+            ticket: '',
+            customers: '',
+            settings:'',
+            sidebarGroups: []
+
         }),
         watch: {
             value: function () {
@@ -122,11 +191,15 @@
             },
             ticket: function () {
                 this.ticket = this.$store.state.lang.lang_map.sidebar.ticket
+                this.customers = this.$store.state.lang.lang_map.sidebar.customers
+                this.settings = this.$store.state.lang.lang_map.sidebar.settings
             }
         },
         mounted() {
             this.getCompanyName()
             this.ticket = this.$store.state.lang.lang_map.sidebar.ticket
+            this.customers = this.$store.state.lang.lang_map.sidebar.customers
+            this.settings = this.$store.state.lang.lang_map.sidebar.settings
         },
         methods: {
             checkRoleByIds(ids) {
