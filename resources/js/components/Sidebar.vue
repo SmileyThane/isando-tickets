293e94b0c7a5bc4ae5784e5896e7c1baf87ecefa
SmileyThane <thane.crios@gmail.com>
2020-09-24 11:01:48 +0300

    rolled back sidebar

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -27,8 +27,8 @@
             <v-list-group
                 prepend-icon="mdi-account"
                 :value="sidebarGroups"
-                active-class="#1d6f7e--text"
-                color="#1d6f7e"
+                active-class="green--text"
+                color="green"
                 multiple
             >
                 <template
@@ -73,9 +73,9 @@
             </v-list-item>
             <v-list-group
                 prepend-icon="mdi-ticket-account"
-                active-class="#1d6f7e--text"
+                active-class="green--text"
                 :value="sidebarGroups"
-                color="#1d6f7e"
+                color="green"
                 multiple
             >
                 <template
@@ -124,9 +124,9 @@
             </v-list-item>
             <v-list-group
                 prepend-icon="mdi-cog"
-                active-class="#1d6f7e--text"
+                active-class="green--text"
                 :value="sidebarGroups"
-                color="#1d6f7e"
+                color="green"
                 multiple
 
             >
