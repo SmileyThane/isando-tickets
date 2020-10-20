f04300503ef02aac0d0e3c4fc1fe97a1766a4ae3
SmileyThane <thane.crios@gmail.com>
2020-10-20 11:18:11 +0300

    fixed sidebar expanding

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -34,6 +34,7 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.home}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+            <hr>
             <v-list-group
                 prepend-icon="mdi-account"
                 :value="sidebarGroups"
@@ -81,6 +82,9 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.teams}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+        </v-list>
+        <hr>
+        <v-list dense>
             <v-list-group
                 prepend-icon="mdi-ticket-account"
                 active-class="green--text"
@@ -132,50 +136,53 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.knowledge_base}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-            <v-list-group
-                prepend-icon="mdi-cog"
-                active-class="green--text"
-                :value="sidebarGroups"
-                color="green"
-                multiple
-
-            >
-                <template
-                    v-slot:activator
-                >
-                    <v-list-item-content>
-
-                        <v-list-item-title>{{settings}}</v-list-item-title>
-                    </v-list-item-content>
-                </template>
-                <v-list-item link to="/company">
-                    <v-list-item-action>
-                        <v-icon>mdi-office-building</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link to="/settings/system" v-if="checkRoleByIds([1,2,3])">
-                    <v-list-item-action>
-                        <v-icon>mdi-folder-cog-outline</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.system_settings}}
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
-                    <v-list-item-action>
-                        <v-icon>mdi-cogs</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.general_settings}}
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-group>
         </v-list>
+        <hr>
+        <v-list dense>
+        <v-list-group
+            prepend-icon="mdi-cog"
+            active-class="green--text"
+            :value="sidebarGroups"
+            color="green"
+            multiple
+
+        >
+            <template
+                v-slot:activator
+            >
+                <v-list-item-content>
+
+                    <v-list-item-title>{{settings}}</v-list-item-title>
+                </v-list-item-content>
+            </template>
+            <v-list-item link to="/company">
+                <v-list-item-action>
+                    <v-icon>mdi-office-building</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/settings/system" v-if="checkRoleByIds([1,2,3])">
+                <v-list-item-action>
+                    <v-icon>mdi-folder-cog-outline</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.system_settings}}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
+                <v-list-item-action>
+                    <v-icon>mdi-cogs</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.general_settings}}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list-group>
+    </v-list>
     </v-navigation-drawer>
 </template>
 
