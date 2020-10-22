7a76939208048834ac503f9352e3a1f2dc5e9d73
SmileyThane <thane.crios@gmail.com>
2020-10-22 14:46:03 +0300

    modified menu button element drawer

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -14,7 +14,9 @@
             </v-list-item-content>
         </v-list-item>
         <v-list-item v-else>
-            <v-list-item-icon><v-img contain max-height="30" max-width="30" :src="companyLogo" left></v-img></v-list-item-icon>
+            <v-list-item-icon>
+                <v-img contain max-height="30" max-width="30" :src="companyLogo" left></v-img>
+            </v-list-item-icon>
             <v-list-item-content>
                 <v-list-item-title class="title">
                     <v-list-item-content>{{ companyName }} | Ticketing</v-list-item-content>
@@ -26,6 +28,12 @@
 
         <v-divider>&nbsp;</v-divider>
         <v-list dense>
+            <v-list-item v-show="localDrawer"
+                         @click.stop="localDrawer = !localDrawer">
+                <v-list-item-action>
+                    <v-icon> mdi-menu</v-icon>
+                </v-list-item-action>
+            </v-list-item>
             <v-list-item link to="/home">
                 <v-list-item-action>
                     <v-icon>mdi-home</v-icon>
@@ -34,7 +42,7 @@
                     <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.home}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-            <hr>
+            <v-divider>&nbsp;</v-divider>
             <v-list-group
                 prepend-icon="mdi-account"
                 :value="sidebarGroups"
@@ -83,7 +91,7 @@
                 </v-list-item-content>
             </v-list-item>
         </v-list>
-        <hr>
+        <v-divider>&nbsp;</v-divider>
         <v-list dense>
             <v-list-group
                 prepend-icon="mdi-ticket-account"
@@ -137,52 +145,52 @@
                 </v-list-item-content>
             </v-list-item>
         </v-list>
-        <hr>
+        <v-divider>&nbsp;</v-divider>
         <v-list dense>
-        <v-list-group
-            prepend-icon="mdi-cog"
-            active-class="green--text"
-            :value="sidebarGroups"
-            color="green"
-            multiple
+            <v-list-group
+                prepend-icon="mdi-cog"
+                active-class="green--text"
+                :value="sidebarGroups"
+                color="green"
+                multiple
 
-        >
-            <template
-                v-slot:activator
             >
-                <v-list-item-content>
+                <template
+                    v-slot:activator
+                >
+                    <v-list-item-content>
 
-                    <v-list-item-title>{{settings}}</v-list-item-title>
-                </v-list-item-content>
-            </template>
-            <v-list-item link to="/company">
-                <v-list-item-action>
-                    <v-icon>mdi-office-building</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link to="/settings/system" v-if="checkRoleByIds([1,2,3])">
-                <v-list-item-action>
-                    <v-icon>mdi-folder-cog-outline</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.system_settings}}
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
-                <v-list-item-action>
-                    <v-icon>mdi-cogs</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.general_settings}}
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list-group>
-    </v-list>
+                        <v-list-item-title>{{settings}}</v-list-item-title>
+                    </v-list-item-content>
+                </template>
+                <v-list-item link to="/company">
+                    <v-list-item-action>
+                        <v-icon>mdi-office-building</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to="/settings/system" v-if="checkRoleByIds([1,2,3])">
+                    <v-list-item-action>
+                        <v-icon>mdi-folder-cog-outline</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.system_settings}}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to="/" v-if="checkRoleByIds([1,2,3])">
+                    <v-list-item-action>
+                        <v-icon>mdi-cogs</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.general_settings}}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
@@ -237,7 +245,7 @@
                 return roleExists
             },
             getCompanyName() {
-               axios.get(`/api/main_company_name`)
+                axios.get(`/api/main_company_name`)
                     .then(
                         response => {
                             this.companyName = response.data.data
