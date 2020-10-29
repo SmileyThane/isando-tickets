4c22d37d8b3e406993b630eb95de0d8fc1881de7
SmileyThane <thane.crios@gmail.com>
2020-10-29 22:47:42 +0300

    added 2fa buuton on login view. restyled login card

--- resources/js/views/Auth/Login.vue
+++ resources/js/views/Auth/Login.vue
@@ -1,5 +1,4 @@
 <template>
-
     <v-content>
         <v-container
             class="fill-height"
@@ -12,60 +11,75 @@
                 <v-col
                     cols="12"
                     sm="8"
-                    md="4"
+                    md="3"
                 >
                     <v-alert
                         type="error"
                         :value="alert"
                     >
-                        Invalid Username or Password
-                    </v-alert>
-                    <v-card class="elevation-12">
-                        <v-toolbar
-                            dense
-                            :color="themeColor"
-                            dark
-                            flat
-                        >
-                            <v-toolbar-title>Authorization</v-toolbar-title>
-                            <v-spacer></v-spacer>
-                        </v-toolbar>
-                        <v-card-text>
-                            <v-form>
-                                <v-text-field
-                                    :color="themeColor"
-                                    label="Login"
-                                    name="email"
-                                    prepend-icon="mdi-account"
-                                    type="text"
-                                    v-model="email"
-                                    required
-                                    autocomplete="new-email"
-                                ></v-text-field>
+                            Invalid Username or Password
+                        </v-alert>
+                        <v-card class="elevation-12" >
+                            <v-toolbar
+                                dense
+                                :color="themeColor"
+                                dark
+                                flat
+                            >
+                                <v-toolbar-title>Authorization</v-toolbar-title>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field
+                                        :color="themeColor"
+                                        label="Login"
+                                        name="email"
+                                        prepend-icon="mdi-account"
+                                        type="text"
+                                        v-model="email"
+                                        required
+                                        autocomplete="new-email"
+                                    ></v-text-field>
 
-                                <v-text-field
-                                    autocomplete="new-password"
-                                    :color="themeColor"
-                                    id="password"
-                                    label="Password"
-                                    name="password"
-                                    prepend-icon="mdi-lock"
-                                    type="password"
-                                    v-model="password"
-                                    required
-                                ></v-text-field>
-                            </v-form>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn :color="themeColor" style="color: white;" @click="handleSubmit">Login</v-btn>
-                        </v-card-actions>
-                    </v-card>
+                                    <v-text-field
+                                        autocomplete="new-password"
+                                        :color="themeColor"
+                                        id="password"
+                                        label="Password"
+                                        name="password"
+                                        prepend-icon="mdi-lock"
+                                        type="password"
+                                        v-model="password"
+                                        required
+                                    ></v-text-field>
+
+                                    <v-tooltip bottom>
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-icon
+                                                color="grey"
+                                                dark
+                                                v-bind="attrs"
+                                                v-on="on"
+                                            >
+                                                mdi-two-factor-authentication
+                                            </v-icon>
+                                        </template>
+                                        <span>Coming soon...</span>
+                                    </v-tooltip>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn :color="themeColor" style="color: white;" @click="handleSubmit">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+
+
                 </v-col>
             </v-row>
         </v-container>
     </v-content>
-
 </template>
 
 <script>
