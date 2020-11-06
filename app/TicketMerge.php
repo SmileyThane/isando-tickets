ea2d5c7a90fba5d09b8cf1048dbfb5f83808ed63
SmileyThane <thane.crios@gmail.com>
2020-11-06 18:40:12 +0300

    added soft delete to ticket merge model

--- app/TicketMerge.php
+++ app/TicketMerge.php
@@ -4,9 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketMerge extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['parent_ticket_id', 'child_ticket_id', 'merged_by_user_id', 'merge_comment'];
 
     public function parentTicketData(): HasOne
