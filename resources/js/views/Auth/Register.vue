81e4ae4bf5328c440947f7bc45dd70b6425f6fb6
SmileyThane <thane.crios@gmail.com>
2020-11-11 17:01:03 +0300

    modified content block

--- resources/js/views/Auth/Register.vue
+++ resources/js/views/Auth/Register.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <v-content>
+    <v-main>
         <v-container
             class="fill-height"
             fluid
@@ -109,7 +109,7 @@
                 </v-col>
             </v-row>
         </v-container>
-    </v-content>
+    </v-main>
 
 </template>
 
