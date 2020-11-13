7584d3bdc671e60f1fa2a1caaef0797932c689d5
Vadim Karacharski <vadim@quasitex.com>
2020-11-13 18:35:59 +0300

    Ident items in sidebar, move Team under Settings

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -57,11 +57,11 @@
                 </v-list-item-content>
             </v-list-item>
             <v-divider>&nbsp;</v-divider>
-            <v-list-group
-                prepend-icon="mdi-account"
-                :value="sidebarGroups"
-                :color="themeColor"
-                multiple
+            <v-list-group class="ml-2"
+                          prepend-icon="mdi-account"
+                          :value="sidebarGroups"
+                          :color="themeColor"
+                          multiple
             >
                 <template
                     v-slot:activator
@@ -95,22 +95,14 @@
                     </v-list-item-content>
                 </v-list-item>
             </v-list-group>
-            <v-list-item link to="/team" v-if="checkRoleByIds([1,2,3])">
-                <v-list-item-action>
-                    <v-icon>mdi-account-box-multiple-outline</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.teams}}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
         </v-list>
         <v-divider>&nbsp;</v-divider>
         <v-list dense>
-            <v-list-group
-                prepend-icon="mdi-ticket-account"
-                :value="sidebarGroups"
-                :color="themeColor"
-                multiple
+            <v-list-group class="ml-2"
+                          prepend-icon="mdi-ticket-account"
+                          :value="sidebarGroups"
+                          :color="themeColor"
+                          multiple
             >
                 <template
                     v-slot:activator
@@ -159,11 +151,11 @@
         </v-list>
         <v-divider>&nbsp;</v-divider>
         <v-list dense>
-        <v-list-group
-            prepend-icon="mdi-cog"
-            :value="sidebarGroups"
-            :color="themeColor"
-            multiple
+            <v-list-group class="ml-2"
+                          prepend-icon="mdi-cog"
+                          :value="sidebarGroups"
+                          :color="themeColor"
+                          multiple
 
             >
                 <template
@@ -180,6 +172,14 @@
                     </v-list-item-action>
                     <v-list-item-content>
                         <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.companies}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link to="/team" v-if="checkRoleByIds([1,2,3])">
+                    <v-list-item-action>
+                        <v-icon>mdi-account-box-multiple-outline</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{this.$store.state.lang.lang_map.sidebar.teams}}</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item link to="/settings/system" v-if="checkRoleByIds([1,2,3])">
@@ -206,98 +206,98 @@
 </template>
 
 <script>
-    import EventBus from './EventBus.vue';
+import EventBus from './EventBus.vue';
 
-    export default {
-        name: "Sidebar",
-        props: {value: {type: Boolean}},
-        data() {
-            return {
-                companyName: '',
-                companyLogo: '',
-                navbarStyle: 1,
-                localDrawer: null,
-                drawer: true,
-                show: true,
-                ticket: '',
-                customers: '',
-                settings: '',
-                sidebarGroups: [],
-                themeColor: this.$store.state.themeColor
-            }
+export default {
+    name: "Sidebar",
+    props: {value: {type: Boolean}},
+    data() {
+        return {
+            companyName: '',
+            companyLogo: '',
+            navbarStyle: 1,
+            localDrawer: null,
+            drawer: true,
+            show: true,
+            ticket: '',
+            customers: '',
+            settings: '',
+            sidebarGroups: [],
+            themeColor: this.$store.state.themeColor
+        }
+    },
+    watch: {
+        value: function () {
+            this.localDrawer = this.value
         },
-        watch: {
-            value: function () {
-                this.localDrawer = this.value
-            },
-            localDrawer: function () {
-                this.$emit('input', this.localDrawer)
-            },
-            $route(to, from) {
-                this.changeAppTitle();
-            },
-            ticket: function () {
-                this.ticket = this.$store.state.lang.lang_map.sidebar.ticket
-                this.customers = this.$store.state.lang.lang_map.sidebar.customers
-                this.settings = this.$store.state.lang.lang_map.sidebar.settings
-            }
+        localDrawer: function () {
+            this.$emit('input', this.localDrawer)
         },
-        mounted() {
-            this.getCompanyName();
-            this.getCompanyLogo();
-            this.getCompanySettings();
-            this.ticket = this.$store.state.lang.lang_map.sidebar.ticket;
-            this.customers = this.$store.state.lang.lang_map.sidebar.customers;
-            this.settings = this.$store.state.lang.lang_map.sidebar.settings;
-            let that = this;
-            EventBus.$on('update-theme-color', function (color) {
-                that.themeColor = color;
+        $route(to, from) {
+            this.changeAppTitle();
+        },
+        ticket: function () {
+            this.ticket = this.$store.state.lang.lang_map.sidebar.ticket
+            this.customers = this.$store.state.lang.lang_map.sidebar.customers
+            this.settings = this.$store.state.lang.lang_map.sidebar.settings
+        }
+    },
+    mounted() {
+        this.getCompanyName();
+        this.getCompanyLogo();
+        this.getCompanySettings();
+        this.ticket = this.$store.state.lang.lang_map.sidebar.ticket;
+        this.customers = this.$store.state.lang.lang_map.sidebar.customers;
+        this.settings = this.$store.state.lang.lang_map.sidebar.settings;
+        let that = this;
+        EventBus.$on('update-theme-color', function (color) {
+            that.themeColor = color;
+        });
+        EventBus.$on('update-navbar-style', function (style) {
+            that.navbarStyle = style;
+        });
+        EventBus.$on('update-navbar-logo', function (logo) {
+            that.companyLogo = logo;
+        });
+    },
+    methods: {
+        checkRoleByIds(ids) {
+            let roleExists = false;
+            ids.forEach(id => {
+                if (roleExists === false) {
+                    roleExists = this.$store.state.roles.includes(id)
+                }
             });
-            EventBus.$on('update-navbar-style', function (style) {
-                that.navbarStyle = style;
-            });
-            EventBus.$on('update-navbar-logo', function (logo) {
-                that.companyLogo = logo;
+            return roleExists
+        },
+        getCompanyName() {
+            axios.get(`/api/main_company_name`)
+                .then(
+                    response => {
+                        this.companyName = response.data.data
+                        this.changeAppTitle();
+                    });
+        },
+        getCompanyLogo() {
+            axios.get(`/api/main_company_logo`).then(response => {
+                response = response.data;
+                if (response.success === true) {
+                    this.companyLogo = response.data;
+                }
             });
         },
-        methods: {
-            checkRoleByIds(ids) {
-                let roleExists = false;
-                ids.forEach(id => {
-                    if (roleExists === false) {
-                        roleExists = this.$store.state.roles.includes(id)
-                    }
-                });
-                return roleExists
-            },
-            getCompanyName() {
-                axios.get(`/api/main_company_name`)
-                    .then(
-                        response => {
-                            this.companyName = response.data.data
-                            this.changeAppTitle();
-                        });
-            },
-            getCompanyLogo() {
-                axios.get(`/api/main_company_logo`).then(response => {
-                    response = response.data;
-                    if (response.success === true) {
-                        this.companyLogo = response.data;
-                    }
-                });
-            },
-            getCompanySettings() {
-                axios.get(`/api/main_company_settings`).then(response => {
-                    response = response.data;
-                    if (response.success === true) {
-                        this.navbarStyle = response.data.hasOwnProperty('navbar_style') ? response.data.navbar_style : 1;
-                    }
-                });
-            },
-            changeAppTitle() {
-                this.$store.state.pageName = this.$store.state.lang.lang_map.sidebar[this.$route.name]
-                document.title = this.companyName + ' | ' + this.$store.state.lang.lang_map.sidebar[this.$route.name]
-            }
+        getCompanySettings() {
+            axios.get(`/api/main_company_settings`).then(response => {
+                response = response.data;
+                if (response.success === true) {
+                    this.navbarStyle = response.data.hasOwnProperty('navbar_style') ? response.data.navbar_style : 1;
+                }
+            });
+        },
+        changeAppTitle() {
+            this.$store.state.pageName = this.$store.state.lang.lang_map.sidebar[this.$route.name]
+            document.title = this.companyName + ' | ' + this.$store.state.lang.lang_map.sidebar[this.$route.name]
         }
     }
+}
 </script>
