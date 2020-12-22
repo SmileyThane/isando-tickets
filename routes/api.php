0921bd780f7099fcc3c1802186aa6feda73cde71
Hleb Prakhnitski <thane.crios@gmail.com>
2020-12-22 06:19:30 +0300

    created new route

--- routes/api.php
+++ routes/api.php
@@ -145,6 +145,7 @@ Route::group(['middleware' => 'auth:api'], function () {
         Route::get('ticket_types', 'API\TicketController@types');
         Route::get('ticket_categories', 'API\TicketController@categories');
         Route::post('merge/ticket', 'API\TicketController@addMerge');
+        Route::delete('merge/ticket/{id}', 'API\TicketController@removeMerge');
         Route::post('link/ticket', 'API\TicketController@addLink');
         Route::post('spam/ticket', 'API\TicketController@markAsSpam');
 
