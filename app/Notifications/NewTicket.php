2dec71075d657fa4fad1f3297b8e0537b29db340
Vadim Karacharski <vadim@quasitex.com>
2020-12-28 12:30:55 +0300

    Add german notifications

--- app/Notifications/NewTicket.php
+++ app/Notifications/NewTicket.php
@@ -17,6 +17,7 @@ class NewTicket extends Notification
     protected $ticket_subject;
     protected $ticket_id;
     protected $from;
+    protected $language;
 
     /**
      * Create a new notification instance.
@@ -25,13 +26,15 @@ class NewTicket extends Notification
      * @param $name
      * @param $ticket_subject
      * @param $ticket_id
+     * @param $language
      */
-    public function __construct($from, $name, $ticket_subject, $ticket_id)
+    public function __construct($from, $name, $ticket_subject, $ticket_id, $language = 'en')
     {
         $this->name = $name;
         $this->ticket_subject = $ticket_subject;
         $this->ticket_id = $ticket_id;
         $this->from = $from;
+        $this->language = $language;
     }
 
     /**
@@ -54,17 +57,31 @@ class NewTicket extends Notification
     public function toMail($notifiable): MailMessage
     {
         Log::info('email sending was started!');
-        return (new MailMessage)
-            ->from(Config::get('mail.from.address'), $this->from)
-            ->subject('New ticket for you!')
-            ->line('Hello ' . $this->name . ',')
-            ->line('You have a new ticket on your profile:' . $this->ticket_subject)
-            ->action('View online', env('APP_URL') . '/ticket/' . $this->ticket_id)
-            ->line('Or respond directly to this email.')
-            ->line('Please do not copy this message in your email response, and do not change subject of this email.
+        if ($this->language == 'en') {
+            return (new MailMessage)
+                ->from(Config::get('mail.from.address'), $this->from)
+                ->subject('New ticket for you!')
+                ->line('Hello ' . $this->name . ',')
+                ->line('You have a new ticket on your profile:' . $this->ticket_subject)
+                ->action('View online', env('APP_URL') . '/ticket/' . $this->ticket_id)
+                ->line('Or respond directly to this email.')
+                ->line('Please do not copy this message in your email response, and do not change subject of this email.
             This may cause delays in processing as your response may not be correctly assigned to your ticket.')
-            ->line('Have a great day ahead!')
-            ->salutation('Regards, ' . $this->from);
+                ->line('Have a great day ahead!')
+                ->salutation('Regards, ' . $this->from);
+        } else {
+            return (new MailMessage)
+                ->from(Config::get('mail.from.address'), $this->from)
+                ->subject('Neues Ticket für Sie!')
+                ->line('Hallo ' . $this->name . ',')
+                ->line('Sie haben ein neues Ticket in Ihrem Profil: ' . $this->ticket_subject)
+                ->action('Online ansehen:', env('APP_URL') . '/ticket/' . $this->ticket_id)
+                ->line('oder antworten Sie direkt auf diese E-Mail.')
+                ->line('Bitte kopieren Sie diese Nachricht nicht in Ihre E-Mail-Antwort und ändern Sie den Betreff dieser E-Mail nicht.
+                Dies kann zu Verzögerungen bei der Bearbeitung führen, da Ihre Antwort möglicherweise nicht korrekt Ihrem Ticket zugewiesen wird.')
+                ->line('Wir wünschen Ihnen einen schönen Tag!')
+                ->salutation('Freundliche Grüsse, ' . $this->from);
+        }
     }
 
     /**
