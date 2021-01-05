1824c0875802c67b2e30b56934df963ebc433108
AlexKovalko <alexkavalko@gmail.com>
2021-01-05 20:39:52 +0300

    Merge - search by contact

--- app/Repository/TicketRepository.php
+++ app/Repository/TicketRepository.php
@@ -72,8 +72,25 @@ class TicketRepository
             $ticketResult->where(
                 static function ($query) use ($request) {
                     if ($request->has('search_param')) {
-                        $key = $request->search_param === 'ID' ? 'id' : 'name';
-                        $query->where($key, 'like', '%' . trim($request->search) . '%');
+                        switch (strtolower($request->search_param)) {
+                            case 'id':
+                                $query->where('id', 'like', '%' . trim($request->search) . '%');
+                                break;
+                            case 'contact':
+                                $search = trim($request->search);
+                                $query->whereHas('contact.userData', function ($q) use ($search) {
+                                    $q->where(function ($_query) use ($search) {
+                                        $_query->where('name', 'LIKE', '%' . $search . '%')
+                                            ->orWhere('surname', 'LIKE', '%' . $search . '%')
+                                            ->orWhereHas('emails', function ($_q) use ($search) {
+                                                $_q->where('email', 'LIKE', '%' . $search . '%');
+                                            });
+                                    });
+                                });
+                                break;
+                            default:
+                                $query->where('name', 'like', '%' . trim($request->search) . '%');
+                        }
                     } else {
                         $query->where('name', 'like', '%' . trim($request->search) . '%')
                             ->orWhere('description', 'like', '%' . trim($request->search) . '%');
