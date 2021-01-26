222db228b2f8d8469c724b35ba99ccc35f5afb9e
Vadim Karacharski <vadim@quasitex.com>
2021-01-26 20:02:35 +0300

    use all emails for email attribute

--- app/User.php
+++ app/User.php
@@ -114,19 +114,22 @@ class User extends Authenticatable
 
     public function getEmailAttribute()
     {
-        $email = $this->emails()->where('email_type', 1)->first();
+        $email = $this->emails()->where('email_type', 1)->orderBy('email_type')->first();
         return $email ? $email->email : null;
     }
 
     public function getEmailIdAttribute()
     {
-        $email = $this->emails()->where('email_type', 1)->first();
+        $email = $this->emails()->where('email_type', 1)->orderBy('email_type')->first();
+        if (!email) {
+            $email = $this->emails()->first();
+        }
         return $email ? $email->id : null;
     }
 
     public function getContactEmailAttribute()
     {
-        return $email = $this->emails()->with('type')->first();
+        return $email = $this->emails()->with('type')->orderBy('email_type')->first();
     }
 
     public function emailSignatures(): MorphMany
