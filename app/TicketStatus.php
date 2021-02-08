9c27023f43ea62c69b57765445b11e746ec0d225
Hleb Prakhnitski <thane.crios@gmail.com>
2021-02-08 04:54:12 +0400

    added translation modifier for ticket status model

--- app/TicketStatus.php
+++ app/TicketStatus.php
@@ -3,10 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class TicketStatus extends Model
 {
     public const OPEN = 2;
     public const CLOSED = 5;
-    //
+
+    protected $langId;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->langId = Auth::user() ? Auth::user()->language_id : 1;
+    }
+
+    public function getNameAttribute()
+    {
+        $translationsArray = Language::find($this->langId)->lang_map;
+        $name = $this->attributes['name'];
+        return $translationsArray->ticket_statuses->$name ?? $this->attributes['name'];
+    }
 }
