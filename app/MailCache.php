7386210ca0804d6bb726f5b93d6102349b44e680
Hleb Prakhnitski <thane.crios@gmail.com>
2021-02-08 05:52:25 +0400

    added new email for receiving

--- app/MailCache.php
+++ app/MailCache.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class MailCache extends Model
 {
     protected $table = 'mail_cache';
-    public const CONTACT_FORM_ADDRESSES = ['contactforms@inax247.com', 'contactforms@inax.ch'];
+    public const CONTACT_FORM_ADDRESSES = ['contactforms@inax247.com', 'contactforms@inax.ch', 'contactforms@inax247.ch'];
 }
