46fe3af7489bc963f458fcab0c7d295d7b070850
Hleb Prakhnitski <thane.crios@gmail.com>
2021-03-05 01:13:08 +0400

    optimized global app component

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -13,9 +13,7 @@
         <v-content v-if="isLoaded"
                        :style="isAuthorized === false ? 'background-image: url(/login_bg.jpg); background-size: cover;' : ''">
                 <speed-panel v-if="isLoaded && isAuthorized === true" v-model="drawer"></speed-panel>
-
                 <router-view></router-view>
-
             </v-content>
         </perfect-scrollbar>
         <appFooter v-if="isAuthorized === true"></appFooter>
