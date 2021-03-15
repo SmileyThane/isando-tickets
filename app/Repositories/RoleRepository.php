a33449d720392b1022d95efc58a07a88f7f53340
Hleb Prakhnitski <thane.crios@gmail.com>
2021-03-15 04:49:17 +0300

    added new method in role repo modified old methods on role repo moved 'all roles' call from user repo to role repo

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -5,11 +5,15 @@ namespace App\Repositories;
 
 
 use App\ModelHasRole;
+use App\Permission;
+use App\Role;
+use App\RoleHasPermission;
 use Illuminate\Http\Request;
+use stdClass;
 
 class RoleRepository
 {
-    public function attach($modelId, $class, $roleId)
+    public function attach($modelId, $class, $roleId): bool
     {
         $newRole = new ModelHasRole();
         $newRole->model_id = $modelId;
@@ -19,7 +23,7 @@ class RoleRepository
         return true;
     }
 
-    public function detach($modelId, $class, $roleId)
+    public function detach($modelId, $class, $roleId): bool
     {
         $attachedRole = ModelHasRole::where([
             'model_id' => $modelId,
@@ -30,8 +34,7 @@ class RoleRepository
         return true;
     }
 
-
-    public function updateRoles(Request $request)
+    public function updateRoles(Request $request): bool
     {
         ModelHasRole::where(['model_id' => $request->company_user_id,
             'model_type' => $request->model_type])->delete();
@@ -43,6 +46,41 @@ class RoleRepository
             );
         }
         return true;
+    }
+
+    public function mergeRolesWithPermissions(array $data): array
+    {
+        $result = [];
+        foreach ($data as $item) {
+            foreach ($item as $key => $value) {
+                if ($value === true) {
+                    $result[] = [
+                        'permission_id' => $item['0']['id'],
+                        'role_id' => $key
+                    ];
+                }
+            }
+        }
+
+        RoleHasPermission::query()->truncate();
+        RoleHasPermission::query()->insert($result);
+        return $this->getRolesWithPermissions();
+    }
+
+    public function getRolesWithPermissions(): array
+    {
+        $result = [];
+        $roles = Role::all();
+        $permissions = Permission::all();
+
+        foreach ($permissions as $key => $permission) {
+            $result[$key] = new stdClass();
+            $result[$key]->{0} = ['id' => $permission->id, 'name' => $permission->name];
+            foreach ($roles as $role) {
+                $result[$key]->{$role->id} = in_array($permission->id, $role->permissionIds(), true);
+            }
+        }
+        return $result;
     }
 
 }
