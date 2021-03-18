cb76a60bf8a90718f32b698a31816f293b092e69
AlexKovalko <alexkavalko@gmail.com>
2021-03-18 16:55:06 +0300

    upd migration

--- database/migrations/2021_03_18_105355_update-tracking-add-morph-realtions.php
+++ database/migrations/2021_03_18_105355_update-tracking-add-morph-realtions.php
@@ -14,8 +14,6 @@ class UpdateTrackingAddMorphRealtions extends Migration
     public function up()
     {
         Schema::table('tracking', static function (Blueprint $table) {
-            $table->dropForeign(['project_id']);
-//            $table->dropIndex('tracking_tracking_projects_id_fk');
             $table->renameColumn('project_id', 'entity_id')->change();
         });
         Schema::table('tracking', static function (Blueprint $table) {
