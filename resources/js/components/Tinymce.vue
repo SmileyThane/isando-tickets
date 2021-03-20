d74e2c9c3d702ee11e20e63256da1e6e8a5bd4eb
Hleb Prakhnitski <thane.crios@gmail.com>
2021-03-20 14:42:08 +0300

    created child customizable component of global tinymce

---
+++ resources/js/components/Tinymce.vue
@@ -0,0 +1,37 @@
+<template>
+    <tinymce
+        :init="{
+         height: 300,
+         menubar: true,
+         plugins: [
+           'advlist autolink lists link image charmap print preview anchor',
+           'searchreplace visualblocks code fullscreen',
+           'insertdatetime media table paste code help wordcount'
+         ],
+         toolbar:
+           'undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help'
+       }"
+        api-key="2v20ajrzng0ftoh79hde6tj4xavm9ej7au8gq939q914sb0w"
+        @input="handleInput"
+    />
+</template>
+
+<script>
+
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+    name: "Tinymce",
+    components: {
+        'tinymce': Editor
+    },
+    methods: {
+        handleInput(e) {
+            // console.log(e);
+            this.$emit('input', e)
+        }
+    }
+}
+</script>
