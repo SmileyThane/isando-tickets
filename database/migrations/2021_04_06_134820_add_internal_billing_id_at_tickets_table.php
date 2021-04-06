cbd354fcdac20aa0fe8de4094c6c16729fb798be
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-06 17:04:47 +0300

    added billedBy relation for internal billing added new column for relation

---
+++ database/migrations/2021_04_06_134820_add_internal_billing_id_at_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInternalBillingIdAtTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', static function (Blueprint $table) {
+            $table->string('internal_billing_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', static function (Blueprint $table) {
+            $table->dropColumn('internal_billing_id');
+        });
+    }
+}
