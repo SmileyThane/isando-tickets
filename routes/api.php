e2a70da3a9f5984b776c4b966fb1b769b2fe4a92
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-06 17:05:27 +0300

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::post('register', 'API\AuthController@register');
 Route::post('login', 'API\AuthController@login');
 Route::post('reset_password', 'API\AuthController@resetPassword');
