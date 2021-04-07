00346f4bf7d9710184cc4547fef081f8d3dcb2c2
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-07 17:30:52 +0300

    udpated internal billing model

--- app/InternalBilling.php
+++ app/InternalBilling.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class InternalBilling extends Model
 {
+    protected $fillable = ['name', 'entity_id', 'entity_type', 'cost', 'currency_id'];
 
     public function billable(): MorphTo
     {
