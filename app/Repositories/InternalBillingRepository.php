f3e3025e56439f8fdc003d2f119e0b984ab18161
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-09 00:38:26 +0300

    handled internal billing parameter from request

--- app/Repositories/InternalBillingRepository.php
+++ app/Repositories/InternalBillingRepository.php
@@ -9,11 +9,10 @@ use App\InternalBilling;
 use App\Role;
 use App\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class InternalBillingRepository
 {
-    public function index($additionalUserIds = null)
+    public function index($additionalUserIds = null, $internalBillingId = null)
     {
         $employee = Auth::user()->employee;
         $entities = [];
@@ -21,26 +20,30 @@ class InternalBillingRepository
             $clients = ClientCompanyUser::query()->where('company_user_id', $employee->id)->get();
             if ($clients) {
                 foreach ($clients->pluck('client_id')->toArray() as $clientId) {
-                    $entities[] = ['entity_type' => Client::class , 'entity_id' => $clientId];
+                    $entities[] = ['entity_type' => Client::class, 'entity_id' => $clientId];
                 }
             }
         }
         if ($employee->hasRoleId(Role::HIGH_PRIVIGIES)) {
             $company = Company::find($employee->company_id);
-            $entities[] = ['entity_type' => Company::class , 'entity_id' => $employee->company_id];
+            $entities[] = ['entity_type' => Company::class, 'entity_id' => $employee->company_id];
             $clientsIds = (new ClientRepository())->getRecursiveChildClientIds($company->clients);
             if ($clientsIds) {
                 foreach ($clientsIds as $clientId) {
-                    $entities[] = ['entity_type' => Client::class , 'entity_id' => $clientId];
+                    $entities[] = ['entity_type' => Client::class, 'entity_id' => $clientId];
                 }
             }
         }
-        $entities[] = ['entity_type' => User::class , 'entity_id' => Auth::id()];
+        $entities[] = ['entity_type' => User::class, 'entity_id' => Auth::id()];
 
         if ($additionalUserIds) {
             foreach (array_unique($additionalUserIds) as $additionalUserId) {
-                $entities[] = ['entity_type' => User::class , 'entity_id' => $additionalUserId];
+                $entities[] = ['entity_type' => User::class, 'entity_id' => $additionalUserId];
             }
+        }
+
+        if ($internalBillingId) {
+            $entities[] = ['id' => $internalBillingId];
         }
 
         $internalBilling = InternalBilling::query();
