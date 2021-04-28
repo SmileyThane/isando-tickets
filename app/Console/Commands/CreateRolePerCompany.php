19943d50844bc636ac17448cf59550b76c5aad61
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-28 05:17:40 +0300

    added new modification for updating roles per related models

--- app/Console/Commands/CreateRolePerCompany.php
+++ app/Console/Commands/CreateRolePerCompany.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Company;
+use App\CompanyUser;
+use App\ModelHasRole;
 use App\Repositories\RoleRepository;
 use App\Role;
+use App\User;
 use Illuminate\Console\Command;
 
 class CreateRolePerCompany extends Command
@@ -45,6 +48,33 @@ class CreateRolePerCompany extends Command
             foreach (Company::all() as $company) {
                 $this->info('-- for company: ' . $company->name);
                 (new RoleRepository())->replicate($role->id, $company->id);
+            }
+        }
+
+        foreach (User::all() as $user) {
+            if ($user->employee) {
+                $roles = $user->employee->roles;
+                foreach ($roles as $role) {
+                    if ($role->company_id === null) {
+                        $newRole = Role::query()->where([
+                            'name' => $role->name,
+                            'company_id' => $user->employee->company_id
+                        ])->first();
+
+                        $modelHasRole = ModelHasRole::where([
+                            'model_id' => $user->employee->id,
+                            'model_type' => CompanyUser::class,
+                            'role_id' => $role->id
+                        ])->delete();
+                        $this->info('Company user: [' . $user->employee->id . '] - ' . $user->name);
+                        $this->info(' -- changed role id from: ' . $role->id . ' to ' . $newRole->id);
+                        ModelHasRole::query()->create([
+                            'model_id' => $user->employee->id,
+                            'model_type' => CompanyUser::class,
+                            'role_id' => $newRole->id
+                        ]);
+                    }
+                }
             }
         }
         return 0;
