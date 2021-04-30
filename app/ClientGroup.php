aea894a5228f1e89d5062f2b68ca3302c5c276f4
Hleb Prakhnitski <thane.crios@gmail.com>
2021-04-30 05:22:39 +0300

    added relations in ClientGroup

--- app/ClientGroup.php
+++ app/ClientGroup.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class ClientGroup extends Model
 {
     protected $fillable = ['name', 'company_id'];
+
+    public function employees()
+    {
+        return $this->hasMany(EmployeeClientGroup::class, 'client_group_id', 'id');
+    }
+
+    public function clients()
+    {
+        return $this->hasMany(ClientGroupHasClient::class, 'client_group_id', 'id');
+    }
 }
