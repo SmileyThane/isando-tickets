ea1a16ed617979ecfb33cf9f6ff6bb2c0ceab5f8
Hleb Prakhnitski <thane.crios@gmail.com>
2021-05-01 19:08:28 +0300

    added relations for ClientGroupHasClient and EmployeeClientGroup models

--- app/ClientGroupHasClient.php
+++ app/ClientGroupHasClient.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ClientGroupHasClient extends Model
 {
     protected $fillable = ['client_id', 'client_group_id'];
+
+    public function client(): HasOne
+    {
+        return $this->hasOne(Client::class, 'id', 'client_id');
+    }
+
+    public function clientGroup(): HasOne
+    {
+        return $this->hasOne(ClientGroup::class, 'id', 'client_group_id');
+    }
 }
