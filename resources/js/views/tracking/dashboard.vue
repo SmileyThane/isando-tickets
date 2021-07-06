58a5f51209e46c0abcb4f214c2286dd0de247927
AlexKovalko <alexkavalko@gmail.com>
2021-07-06 13:03:04 +0300

    Top projects - add narrative if none billable

--- resources/js/views/tracking/dashboard.vue
+++ resources/js/views/tracking/dashboard.vue
@@ -71,33 +71,41 @@
                             outlined
                         >
                             <v-card-title>Top project</v-card-title>
+                            <template v-if="data.topProjects.length">
+                                <div
+                                    v-for="(item, index) in data.topProjects"
+                                    :key="item.id"
+                                    class="d-flex flex-grow-1 flex-column mb-4 mx-4 mt-0"
+                                    style="min-width: 400px;"
+                                >
+                                    <div class="flex-grow-1 text-left" style="width: 100%; overflow: hidden">
+                                        <strong>{{index+1}}.</strong>
+                                        <strong v-if="!item.project_name">Without project</strong>
+                                        <strong v-else>
+                                            <span v-if="item.project_name">{{item.project_name}}</span>
+                                            / <span v-if="item.client_name">{{item.client_name}}</span>
+                                        </strong>
+                                    </div>
+                                    <div class="d-flex flex-row" style="width: 100%">
+                                        <div class="d-inline-flex flex-grow-1 text-left" style="width: 50%">
+                                            Total time: {{$helpers.time.convertSecToTime(item.duration, false)}} h
+                                        </div>
+                                        <div
+                                            class="d-inline-flex flex-row-reverse flex-grow-1 text-right"
+                                            style="width: 50%"
+                                            v-if="$helpers.auth.checkPermissionByIds([59])"
+                                        >
+                                            <span v-if="currentCurrency">{{currentCurrency.symbol}}</span>
+                                            <span>Revenue:&nbsp;{{$helpers.numbers.numberFormat(item.revenue, 2)}}&nbsp;</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </template>
                             <div
-                                v-for="(item, index) in data.topProjects"
-                                :key="item.id"
-                                class="d-flex flex-grow-1 flex-column mb-4 mx-4 mt-0"
-                                style="min-width: 400px;"
+                                v-else
+                                class="font-italic top-projects__info"
                             >
-                                <div class="flex-grow-1 text-left" style="width: 100%; overflow: hidden">
-                                    <strong>{{index+1}}.</strong>
-                                    <strong v-if="!item.project_name">Without project</strong>
-                                    <strong v-else>
-                                        <span v-if="item.project_name">{{item.project_name}}</span>
-                                        / <span v-if="item.client_name">{{item.client_name}}</span>
-                                    </strong>
-                                </div>
-                                <div class="d-flex flex-row" style="width: 100%">
-                                    <div class="d-inline-flex flex-grow-1 text-left" style="width: 50%">
-                                        Total time: {{$helpers.time.convertSecToTime(item.duration, false)}} h
-                                    </div>
-                                    <div
-                                        class="d-inline-flex flex-row-reverse flex-grow-1 text-right"
-                                        style="width: 50%"
-                                        v-if="$helpers.auth.checkPermissionByIds([59])"
-                                    >
-                                        <span v-if="currentCurrency">{{currentCurrency.symbol}}</span>
-                                        <span>Revenue:&nbsp;{{$helpers.numbers.numberFormat(item.revenue, 2)}}&nbsp;</span>
-                                    </div>
-                                </div>
+                                No billable projects were tracked in the selected period.
                             </div>
                         </v-card>
                     </template>
@@ -165,6 +173,16 @@
         </div>
     </v-container>
 </template>
+
+<style>
+.top-projects__info {
+    position: relative;
+    top: 30%;
+    margin: 0 auto;
+    padding: 0 50px;
+    width: 335px;
+}
+</style>
 
 <script>
 import EventBus from "../../components/EventBus";
@@ -334,7 +352,7 @@ export default {
             return Object.values(items).map(i => ({ ...i, percent: (i.count / totalTime * 100).toFixed(2) }));
         },
         getColor() {
-            return this.$helpers.color.genRandomColor();
+            return '#' + Math.floor(Math.random()*16777215).toString(16).substr(0, 6);
         }
     },
     watch: {
