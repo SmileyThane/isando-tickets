563c528499be3b59e3bf2ec61d62476d18a3d141
AlexKovalko <alexkavalko@gmail.com>
2021-07-08 14:37:00 +0300

    fix bug

--- app/TrackingTimesheet.php
+++ app/TrackingTimesheet.php
@@ -82,6 +82,22 @@ class TrackingTimesheet extends Model
     public static function boot() {
         parent::boot();
 
+        static::retrieved(function($trackingTimesheet) {
+            if (!$trackingTimesheet->Times()->count() < 7) {
+//                $trackingTimesheet->Times()->delete();
+                $daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+                foreach ($daysOfWeek as $index => $dayOfWeek) {
+                    $time = new TrackingTimesheetTime();
+                    $time->type = TrackingTimesheetTime::TYPE_WORK;
+                    $time->date = Carbon::parse($trackingTimesheet->from)->addDays($index)->format('Y-m-d');
+                    $time->time = '00:00:00';
+                    $time->description = '';
+                    $time->timesheet_id = $trackingTimesheet->id;
+                    $time->save();
+                }
+            }
+        });
+
         static::deleting(function($trackingTimesheet) {
             Tracking::where('timesheet_id', '=', $trackingTimesheet->id)->update(['timesheet_id' => null]);
             $trackingTimesheet->Times()->delete();
