3313058ba75cc57f871529e889ba4fd9d72f0555
Hleb Prakhnitski <thane.crios@gmail.com>
2022-05-31 22:58:33 +0400

    improved permission models

--- app/RoleHasPermission.php
+++ app/RoleHasPermission.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoleHasPermission extends Model
 {
-    protected $fillable = ['role_id', 'permission_id'];
-
     public $timestamps = false;
+    protected $fillable = ['role_id', 'permission_id'];
 
 }
