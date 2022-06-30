b83bf22f5075afb1c5e273dce7a83a6272325ec3
Hleb Prakhnitski <thane.crios@gmail.com>
2022-06-30 22:39:17 +0400

    repo improvement

--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -21,7 +21,6 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Validator;
 use Throwable;
 
