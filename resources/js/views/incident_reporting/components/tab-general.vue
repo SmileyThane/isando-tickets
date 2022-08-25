3d3f080a7f78ee2132e9017aad9bc1c47d8d46cc
Hleb Prakhnitski <thane.crios@gmail.com>
2022-08-25 16:41:39 +0400

    re-created IR AB general tab

--- resources/js/views/incident_reporting/components/tab-general.vue
+++ resources/js/views/incident_reporting/components/tab-general.vue
@@ -2,6 +2,7 @@
     <v-card class="my-2">
         <v-row>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
+                <label>Categories:</label>
                 <v-select
                     v-if="isEditable"
                     class="small"
@@ -14,16 +15,26 @@
                     outlined
                     hide-details
                 ></v-select>
-                <div v-else>
-                    <p v-for="category in item.categories">
-                        {{category}}
-                    </p>
-
+                <div
+                    v-else
+                    v-for="category in $store.getters['IncidentReporting/getSelectedIR'].categories"
+                >
+                    <v-chip
+                            class="float-right text-uppercase"
+                            label
+                            style="margin: 5px;"
+                            :color="themeBgColor"
+                            :textColor="$helpers.color.invertColor(themeBgColor)"
+                    >
+                        {{category.name}}
+                    </v-chip>
                 </div>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12"></v-col>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
+                <label v-if="$store.getters['IncidentReporting/getSelectedIR'].clients.length > 0">Clients:</label>
                 <v-select
+                    v-if="isEditable"
                     class=""
                     placeholder="Clients"
                     :items="$store.getters['RiskRepository/getClients']"
@@ -33,41 +44,120 @@
                     outlined
                     hide-details
                 ></v-select>
+                <div
+                    v-else
+                    v-for="client in $store.getters['IncidentReporting/getSelectedIR'].clients"
+                >
+                    <v-chip
+                        class="float-right text-uppercase"
+                        label
+                        style="margin: 5px;"
+                        :color="themeBgColor"
+                        :textColor="$helpers.color.invertColor(themeBgColor)"
+                    >
+                        {{client.name}}
+                    </v-chip>
+                </div>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12" class="pb-0">
-                <v-checkbox class="mt-0"
+                <v-checkbox v-if="isEditable" class="mt-0"
                     label="Include child organizations"
                 ></v-checkbox>
             </v-col>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
+                <label v-if="$store.getters['IncidentReporting/getSelectedIR'].stage_monitoring !== null">Valid from Stage Monitoring:</label>
                 <v-select
+                    v-if="isEditable"
                     class=""
                     placeholder="Valid from Stage Monitoring"
+                    :items="$store.getters['RiskRepository/getClients']"
+                    item-value="id"
+                    item-text="name"
                     dense
                     outlined
                     hide-details
                 ></v-select>
+                <div
+                    v-else
+                >
+                    <v-chip
+                        class="float-right text-uppercase"
+                        label
+                        style="margin: 5px;"
+                        :color="themeBgColor"
+                        :textColor="$helpers.color.invertColor(themeBgColor)"
+                    >
+                        {{
+                            $store.getters['IncidentReporting/getSelectedIR'].stage_monitoring ?
+                                $store.getters['IncidentReporting/getSelectedIR'].stage_monitoring.name :
+                                ''
+                        }}
+                    </v-chip>
+                </div>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12"></v-col>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
+                <label v-if="$store.getters['IncidentReporting/getSelectedIR'].priority">Importance:</label>
                 <v-select
+                    v-if="isEditable"
                     class=""
                     placeholder="Importance"
                     :items="$store.getters['RiskRepository/getImportance']"
+                    item-value="id"
+                    item-text="name"
                     dense
                     outlined
                     hide-details
                 ></v-select>
+                <div
+                    v-else
+                >
+                    <v-chip
+                        class="float-right text-uppercase"
+                        label
+                        style="margin: 5px;"
+                        :color="themeBgColor"
+                        :textColor="$helpers.color.invertColor(themeBgColor)"
+                    >
+                        {{
+                            $store.getters['IncidentReporting/getSelectedIR'].priority ?
+                                $store.getters['IncidentReporting/getSelectedIR'].priority.name :
+                                ''
+                        }}
+                    </v-chip>
+                </div>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12"></v-col>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
+                <label v-if="$store.getters['IncidentReporting/getSelectedIR'].access">Access:</label>
                 <v-select
+                    v-if="isEditable"
                     class=""
-                    placeholder="Access - all"
+                    placeholder="Access"
+                    :items="$store.getters['RiskRepository/getImportance']"
+                    item-value="id"
+                    item-text="name"
                     dense
                     outlined
                     hide-details
                 ></v-select>
+                <div
+                    v-else
+                >
+                    <v-chip
+                        class="float-right text-uppercase"
+                        label
+                        style="margin: 5px;"
+                        :color="themeBgColor"
+                        :textColor="$helpers.color.invertColor(themeBgColor)"
+                    >
+                        {{
+                            $store.getters['IncidentReporting/getSelectedIR'].access ?
+                                $store.getters['IncidentReporting/getSelectedIR'].access.name :
+                                ''
+                        }}
+                    </v-chip>
+                </div>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12"></v-col>
             <v-col cols="6" xl="4" lg="6" md="6" sm="12" class="pb-0">
@@ -76,7 +166,7 @@
                     label="Description"
                     auto-grow
                     outlined
-                    value=""
+                    :value="$store.getters['IncidentReporting/getSelectedIR'].description"
                 ></v-textarea>
             </v-col>
             <v-col cols="6" xl="8" lg="6" md="6" sm="12"></v-col>
@@ -85,12 +175,16 @@
 </template>
 
 <script>
+import EventBus from "../../../components/EventBus";
+
 export default {
     name: 'incident-tab-general',
     data() {
         return {
             isEditable: false,
-            item: {}
+            themeFgColor: this.$store.state.themeFgColor,
+            themeBgColor: this.$store.state.themeBgColor,
+
         }
     },
     mounted() {
@@ -98,7 +192,14 @@ export default {
         this.$store.dispatch('RiskRepository/callGetArticles');
         this.$store.dispatch('RiskRepository/callGetClients');
         this.$store.dispatch('RiskRepository/callGetImportance');
-        this.item = this.$store.getters['IncidentReporting/getSelectedIR']
+        const that = this
+        EventBus.$on('update-theme-fg-color', function (color) {
+            that.themeFgColor = color;
+        });
+        EventBus.$on('update-theme-bg-color', function (color) {
+            that.themeBgColor = color;
+        });
+
     },
     methods: {
 
