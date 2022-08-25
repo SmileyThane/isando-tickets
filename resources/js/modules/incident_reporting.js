cb714110ab9fdd245376304aaba259a537b3b217
Hleb Prakhnitski <thane.crios@gmail.com>
2022-08-25 15:14:42 +0400

    updated incident_reporting store

--- resources/js/modules/incident_reporting.js
+++ resources/js/modules/incident_reporting.js
@@ -1,19 +1,44 @@
 export default {
     namespaced: true,
     state: {
+        selectedIR: {
+            id: 0,
+            version: ''
+        },
+        IR: [],
         search: '',
         searchWhere: [1,2,3],
         activeTags: [],
     },
     actions: {
-
+        callGetIR({ commit }) {
+            return axios.get(`/api/ir`, {
+                params: {}
+            }).then(({ status, data: { data, success } }) => {
+                if (status === 200 && success) {
+                    commit('setIR', data)
+                    return Promise.resolve(data)
+                }
+                commit('setIR', [])
+                return Promise.reject([])
+            });
+        },
+        callSetSelectedIR({commit}, data) {
+            commit('setSelectedIR', data)
+        }
     },
     mutations: {
+        setIR: (state, data) => state.IR = data,
+        setSelectedIR: (state, data) => {
+            state.selectedIR = data
+        },
         setSearch: (state, data) => state.search = data,
         setSearchWhere: (state, data) => state.searchWhere = data,
         setActiveTags: (state, data) => state.activeTags = data,
     },
     getters: {
+        getIR: state => state.IR,
+        getSelectedIR: state => state.selectedIR,
         getSearch: state => state.search,
         getSearchWhere: state => state.searchWhere,
         getActiveTags: state => state.activeTags,
