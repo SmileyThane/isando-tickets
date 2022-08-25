367a4154c57e55d86cf8852e3c4e8123227a5b5c
Hleb Prakhnitski <thane.crios@gmail.com>
2022-08-25 16:41:11 +0400

    updated IR action relation to user

--- app/IncidentReportingAction.php
+++ app/IncidentReportingAction.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class IncidentReportingAction extends Model
 {
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function assignee()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
