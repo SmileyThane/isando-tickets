09d0222c870514e188fdeab1293984f9dd994583
Hleb Prakhnitski <thane.crios@gmail.com>
2022-08-31 22:46:01 +0400

    fixed search action-boards view

--- resources/js/views/incident_reporting/components/tab-action-boards.vue
+++ resources/js/views/incident_reporting/components/tab-action-boards.vue
@@ -8,10 +8,10 @@
 
             <v-col cols="12">
                 <v-card
-                    class="mx-auto mb-2"
-                    outlined
                     v-for="action in $store.getters['IncidentReporting/getSelectedIR'].actions"
                     :key="action.id"
+                    class="mx-auto mb-2"
+                    outlined
                 >
                     <v-list-item three-line>
                         <v-list-item-content>
@@ -27,7 +27,7 @@
                                 <v-col cols="2">
                                     {{ action.assignee ? action.assignee.email : 'not assinged' }}
                                 </v-col>
-                                <v-col cols="1" class="text-right">
+                                <v-col class="text-right" cols="1">
                                     <v-menu
                                         bottom
                                         left
