43b1fceabd64f148607f1b7084bc6051464d3c27
Hleb Prakhnitski <thane.crios@gmail.com>
2022-09-06 18:50:02 +0400

    added enum parameters for IR action deadline options

--- app/IncidentReportingAction.php
+++ app/IncidentReportingAction.php
@@ -8,6 +8,9 @@ class IncidentReportingAction extends Model
 {
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 
+    const DEADLINE_TIME_INDICATOR = ['before', 'after'];
+    const DEADLINE_TIME_PARAMETER = ['seconds', 'minutes', 'hours', 'days', 'weeks', 'months', 'years'];
+
     public function assignee()
     {
         return $this->hasOne(User::class, 'id', 'user_id');
