1dbfb1617c591b50bd65397ab889e2223033ed99
Hleb Prakhnitski <thane.crios@gmail.com>
2022-09-23 23:22:48 +0400

    improved currencies handling

--- resources/js/modules/currencies.js
+++ resources/js/modules/currencies.js
@@ -5,42 +5,42 @@ export default {
         search: null
     },
     actions: {
-        getCurrencyList({commit}, { search }) {
+        getCurrencyList({commit}, {search}) {
             commit('SET_SEARCH', search)
             const queryParams = new URLSearchParams({
                 search: search ?? ''
             });
-            return axios.get(`/api/currencies?${queryParams.toString()}`, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data } }) => {
+            return axios.get(`/api/currencies?${queryParams.toString()}`, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data}}) => {
                     if (success) {
                         commit('GET_CURRENCIES', data)
                     }
                     return success;
                 })
         },
-        createCurrency({commit, dispatch, state}, { name, slug, symbol }) {
-            return axios.post(`/api/currencies`, { name, slug, symbol }, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data } }) => {
+        createCurrency({commit, dispatch, state}, {name, slug, symbol}) {
+            return axios.post(`/api/currencies`, {name, slug, symbol}, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data}}) => {
                     if (success) {
-                        dispatch('getCurrencyList', { search: state.search });
+                        dispatch('getCurrencyList', {search: state.search});
                     }
                     return success;
                 })
         },
-        updateCurrency({commit, dispatch, state}, { id, name, slug, symbol }) {
-            return axios.patch(`/api/currencies/${id}`, { name, slug, symbol }, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data } }) => {
+        updateCurrency({commit, dispatch, state}, {id, name, slug, symbol}) {
+            return axios.patch(`/api/currencies/${id}`, {name, slug, symbol}, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data}}) => {
                     if (success) {
-                        dispatch('getCurrencyList', { search: state.search });
+                        dispatch('getCurrencyList', {search: state.search});
                     }
                     return success;
                 })
         },
         removeCurrency({commit, dispatch, state}, id) {
-            return axios.delete(`/api/currencies/${id}`, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data } }) => {
+            return axios.delete(`/api/currencies/${id}`, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data}}) => {
                     if (success) {
-                        dispatch('getCurrencyList', { search: state.search });
+                        dispatch('getCurrencyList', {search: state.search});
                     }
                     return success;
                 })
