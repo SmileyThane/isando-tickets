77db48dca3e053e757de764f4748fd2c4cd509a8
Hleb Prakhnitski <thane.crios@gmail.com>
2022-09-23 23:23:09 +0400

    improved products handling

--- resources/js/modules/products.js
+++ resources/js/modules/products.js
@@ -4,15 +4,15 @@ export default {
         products: []
     },
     actions: {
-        getProductList({commit, state}, { search, force = false }) {
+        getProductList({commit, state}, {search, force = false}) {
             if (state.products.length && !search && !force) {
                 return state.products;
             } else {
                 const queryParams = new URLSearchParams({
                     search: search ?? ''
                 });
-                axios.get(`/api/product?${queryParams.toString()}`, { retry: 5, retryDelay: 1000 })
-                    .then(({ data: { success, data: products } }) => {
+                axios.get(`/api/product?${queryParams.toString()}`, {retry: 5, retryDelay: 1000})
+                    .then(({data: {success, data: products}}) => {
                         if (success) {
                             commit('GET_PRODUCTS', products.data)
                         }
