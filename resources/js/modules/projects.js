83fc4bdb472ebc200936399fd3ba9f7e9d0bd338
Hleb Prakhnitski <thane.crios@gmail.com>
2022-09-23 23:23:18 +0400

    improved projects handling

--- resources/js/modules/projects.js
+++ resources/js/modules/projects.js
@@ -12,8 +12,8 @@ export default {
             commit('SET_FILTER', params);
             if (params && !params.search) params.search = '';
             const queryParams = new URLSearchParams(params);
-            return axios.get(`/api/tracking/projects?${queryParams.toString()}`, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data } }) => {
+            return axios.get(`/api/tracking/projects?${queryParams.toString()}`, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data}}) => {
                     if (success) {
                         commit('GET_PROJECTS', data.data)
                         commit('GET_TREE_PROJECTS', data.data)
@@ -22,8 +22,8 @@ export default {
                 })
         },
         createProject({commit, dispatch}, project) {
-            return axios.post('/api/tracking/projects', project, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { success, data: project } }) => {
+            return axios.post('/api/tracking/projects', project, {retry: 5, retryDelay: 1000})
+                .then(({data: {success, data: project}}) => {
                     if (success) {
                         dispatch('getProjectList');
                         return project;
@@ -31,16 +31,16 @@ export default {
                 })
         },
         toggleFavorite({commit, dispatch, state}, project) {
-            return axios.patch(`/api/tracking/projects/${project.id}/favorite`, null, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { data, success } }) => {
+            return axios.patch(`/api/tracking/projects/${project.id}/favorite`, null, {retry: 5, retryDelay: 1000})
+                .then(({data: {data, success}}) => {
                     if (success) {
                         dispatch('getProjectList', state.filter);
                     }
                 });
         },
         toggleArchive({commit, dispatch, state}, project) {
-            return axios.patch(`/api/tracking/projects/${project.id}/archive`, null, { retry: 5, retryDelay: 1000 })
-                .then(({ data: { data, success } }) => {
+            return axios.patch(`/api/tracking/projects/${project.id}/archive`, null, {retry: 5, retryDelay: 1000})
+                .then(({data: {data, success}}) => {
                     if (success) {
                         dispatch('getProjectList', state.filter);
                     }
@@ -48,7 +48,7 @@ export default {
         },
         removeArchive({commit, dispatch, state}, project) {
             return axios.delete(`/api/tracking/projects/${project.id}`)
-                .then(({ data: { data, success } }) => {
+                .then(({data: {data, success}}) => {
                     if (success) {
                         dispatch('getProjectList', state.filter);
                     }
@@ -57,19 +57,19 @@ export default {
     },
     mutations: {
         GET_PROJECTS(state, projects) {
-            state.projects = projects.sort((a,b) => b.is_favorite - a.is_favorite);
+            state.projects = projects.sort((a, b) => b.is_favorite - a.is_favorite);
         },
         GET_TREE_PROJECTS(state, projects) {
             let clients = [];
             projects.forEach(i => {
                 if (!clients.find(x => x.id === i.client.id)) {
-                    clients.push({ ...i.client, projects: [] })
+                    clients.push({...i.client, projects: []})
                 }
             })
             clients = clients.map(client => {
                 client.projects = projects.filter(i => i.client.id === client.id)
-                    .sort((a,b) => (a.name ? a.name.toLowerCase() : a.name) - (b.name ? b.name.toLowerCase() : b.name))
-                    .sort((a,b) => b.is_favorite - a.is_favorite);
+                    .sort((a, b) => (a.name ? a.name.toLowerCase() : a.name) - (b.name ? b.name.toLowerCase() : b.name))
+                    .sort((a, b) => b.is_favorite - a.is_favorite);
                 return client;
             })
             state.treeProjects = clients
