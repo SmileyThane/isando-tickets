c7d44e8e67dcd4aa08c856ecd0d73d2aee73d6c3
Hleb Prakhnitski <thane.crios@gmail.com>
2022-10-13 19:14:04 +0400

    updated models

--- app/IncidentReporting/ImpactPotential.php
+++ app/IncidentReporting/ImpactPotential.php
@@ -5,6 +5,7 @@ namespace App\IncidentReporting;
 class ImpactPotential extends ReferenceBook
 {
     protected $table = 'incident_reporting_impact_potentials';
+    protected $fillable =['name', 'name_de', 'position', 'color'];
 
     public static function boot() {
         parent::boot();
