05d460ceaaeaf5c41f0b8fb5cbf643928f3e4fbc
Hleb Prakhnitski <thane.crios@gmail.com>
2022-11-14 22:13:52 +0400

    fixed selection updated build

--- resources/js/views/knowledge_base/create.vue
+++ resources/js/views/knowledge_base/create.vue
@@ -346,7 +346,7 @@ export default {
                     value: 'independent'
                 }
             ],
-            childCategoriesSelectedItem: 'independent'
+            childCategoriesSelectedItem: 'leaf'
         }
     },
     mounted() {
