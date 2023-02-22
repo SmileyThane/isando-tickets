3b0e4903c287be9902e5c8d658b2cc30450f5aa3
Hleb Prakhnitski <thane.crios@gmail.com>
2023-02-22 10:54:48 +0400

    updated create ticket view

--- resources/js/views/ticket/create.vue
+++ resources/js/views/ticket/create.vue
@@ -379,10 +379,12 @@
                                                                 item-value="id"
                                                             >
                                                                 <template slot="selection" slot-scope="data">
-                                                                    <v-icon left small v-text="data.item.icon"></v-icon> {{ $helpers.i18n.localized(data.item) }}
+                                                                    <v-icon left small v-text="data.item.icon"></v-icon>
+                                                                    {{ $helpers.i18n.localized(data.item) }}
                                                                 </template>
                                                                 <template slot="item" slot-scope="data">
-                                                                    <v-icon left small v-text="data.item.icon"></v-icon> {{ $helpers.i18n.localized(data.item) }}
+                                                                    <v-icon left small v-text="data.item.icon"></v-icon>
+                                                                    {{ $helpers.i18n.localized(data.item) }}
                                                                 </template>
                                                             </v-select>
                                                         </v-col>
