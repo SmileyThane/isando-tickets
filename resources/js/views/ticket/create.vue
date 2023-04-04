e236dbf2930d5fdbe93ad3d991eaeb737e58761f
Hleb Prakhnitski <thane.crios@gmail.com>
2023-04-04 12:26:01 +0400

    minor fix

--- resources/js/views/ticket/create.vue
+++ resources/js/views/ticket/create.vue
@@ -81,18 +81,20 @@
                                                 <div class="row">
                                                     <div class="col-md-12">
                                                         <v-text-field
+                                                            v-model="ticketForm.name"
                                                             :color="themeBgColor"
                                                             :item-color="themeBgColor"
                                                             :label="langMap.ticket.subject"
-                                                            v-model="ticketForm.name"
                                                             hide-details
                                                         ></v-text-field>
                                                     </div>
                                                     <div class="col-md-4">
                                                         <v-select
-                                                            :label="langMap.ticket.priority"
+                                                            v-model="ticketForm.priority_id"
                                                             :color="themeBgColor"
                                                             :item-color="themeBgColor"
+                                                            :items="priorities"
+                                                            :label="langMap.ticket.priority"
                                                             item-text="name"
                                                             item-value="id"
                                                             :items="priorities"
