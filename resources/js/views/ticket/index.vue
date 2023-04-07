27e11ee9001005191f003a06f51f8471384d2cf0
Hleb Prakhnitski <thane.crios@gmail.com>
2023-04-07 12:29:01 +0400

    improved icons displaying on tickets view

--- resources/js/views/ticket/index.vue
+++ resources/js/views/ticket/index.vue
@@ -236,9 +236,9 @@
                 </v-pagination>
             </template>
             <template v-slot:item.ticket_type_id="{ item }">
-                <v-icon v-if="item.ticket_type.icon" small :title="$helpers.i18n.localized(item.ticket_type)"
+                <v-icon v-if="item.ticket_type.icon" :title="$helpers.i18n.localized(item.ticket_type)" small
                         v-text="item.ticket_type.icon"/>
-                <v-icon v-else small :title="$helpers.i18n.localized(item.ticket_type)">mdi-alert</v-icon>
+                <v-icon v-else :title="$helpers.i18n.localized(item.ticket_type)" small>mdi-alert</v-icon>
             </template>
             <template v-slot:item.status.name="{ item }">
                 <v-badge :color="item.status.color" dot inline @click="showItem(item)">
