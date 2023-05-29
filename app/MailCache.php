33c12b65b8a2ab885629cd18384a620fd17eb81e
Hleb Prakhnitski <thane.crios@gmail.com>
2023-05-29 11:11:43 +0400

    created config for cached addresses for email receiving

--- app/MailCache.php
+++ app/MailCache.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MailCache extends Model
 {
     public const CONTACT_FORM_ADDRESSES = [
-        'email@inax247.ch ',
+        'email@inax247.ch',
         'contactforms@inax247.com',
         'contactforms@inax.ch',
         'contactforms@inax247.ch',
