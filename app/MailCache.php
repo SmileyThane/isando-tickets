d07675a6189d48e376131f4650b60a332c1ab1a7
Hleb Prakhnitski <thane.crios@gmail.com>
2023-05-30 20:10:16 +0400

    minor refactoring

--- app/MailCache.php
+++ app/MailCache.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MailCache extends Model
 {
-    public const CONTACT_FORM_ADDRESSES = [
-        'email@inax247.ch',
-        'contactforms@inax247.com',
-        'contactforms@inax.ch',
-        'contactforms@inax247.ch',
-        'support@inax247.com'
-    ];
-
     protected $table = 'mail_cache';
 }
