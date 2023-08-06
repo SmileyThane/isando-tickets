454019a45537072490a6b675822c4bbd66bd951b
vladward <vladward13@gmail.com>
2023-08-06 13:03:51 +0300

    fix line of requests at mount app

--- resources/js/app.js
+++ resources/js/app.js
@@ -82,11 +82,16 @@ const app = new Vue({
             this.$store.state.permissions =
                 this.$store.state.lang = null;
         store.dispatch('getLanguage');
-        store.dispatch('getRoles');
-        store.dispatch('getPermissions');
         store.dispatch('getThemeBgColor');
         store.dispatch('getAppVersion');
+        this.loadRolesPermissions();
         store.dispatch('getMainCompany');
-        store.dispatch('KbTypes/getKbTypes');
+    },
+    methods: {
+        async loadRolesPermissions () {
+            await store.dispatch('getPermissions');
+            await store.dispatch('KbTypes/getKbTypes');
+            await store.dispatch('getRoles');
+        }
     }
 });
