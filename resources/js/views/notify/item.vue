73e7ad2d01aa74fb4aff0b3423656707d938803f
Hleb Prakhnitski <thane.crios@gmail.com>
2023-10-02 18:05:15 +0400

    fix

--- resources/js/views/notify/item.vue
+++ resources/js/views/notify/item.vue
@@ -94,12 +94,13 @@
                 </v-spacer>
                 <v-card class="elevation-12">
                     <v-toolbar
-                        dense
                         :color="themeBgColor"
                         dark
+                        dense
                         flat
                     >
-                        <v-toolbar-title :style="`color: ${themeFgColor};`">{{langMap.notification.attributes}}</v-toolbar-title>
+                        <v-toolbar-title :style="`color: ${themeFgColor};`">{{ langMap.notification.attributes }}
+                        </v-toolbar-title>
                         <v-spacer></v-spacer>
                     </v-toolbar>
                     <v-card-text>
