47000606a1c7e8437a9ca88997906ed99fb0fd4d
Hleb Prakhnitski <thane.crios@gmail.com>
2023-10-29 18:17:16 +0400

    global refactoring for notification views

--- resources/js/views/notify/sent.vue
+++ resources/js/views/notify/sent.vue
@@ -37,12 +37,13 @@
 
                 <v-card class="elevation-12">
                     <v-toolbar
-                        dense
                         :color="themeBgColor"
                         dark
+                        dense
                         flat
                     >
-                        <v-toolbar-title :style="`color: ${themeFgColor};`">{{langMap.notification.attributes}}</v-toolbar-title>
+                        <v-toolbar-title :style="`color: ${themeFgColor};`">{{ langMap.notification.attributes }}
+                        </v-toolbar-title>
                         <v-spacer></v-spacer>
                     </v-toolbar>
                     <v-card-text>
