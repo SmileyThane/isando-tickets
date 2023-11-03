3df64e6d8b5e2af05976a63f0d58e685113cfa27
Hleb Prakhnitski <thane.crios@gmail.com>
2023-11-03 18:22:18 +0400

    tracking filters refactoring

--- app/Http/Controllers/API/Tracking/Filters/Coworkers/Projects.php
+++ app/Http/Controllers/API/Tracking/Filters/Coworkers/Projects.php
@@ -14,19 +14,21 @@ class Projects extends Filter
         if (!is_null($values) && $values !== 'null') {
             $trackingProjects = TrackingProject::whereIn('id', explode(',', $values))->select('company_id', 'team_id')
                 ->get()
-                ->unique(function($item) { return $item->company_id . $item->team_id; })->values()->all();
-                $builder->where(function ($query) use ($trackingProjects) {
-                    foreach ($trackingProjects as $project) {
-                        $query->orWhere(function($subQuery) use ($project) {
-                            $subQuery->orWhere('company_id', '=', $project->company_id);
-                            if (!is_null($project->team_id)) {
-                                $subQuery->whereHas('assignedToTeams', function ($q) use ($project) {
-                                    $q->where('team_id', '=', $project->team_id);
-                                });
-                            }
-                        });
-                    }
-                });
+                ->unique(function ($item) {
+                    return $item->company_id . $item->team_id;
+                })->values()->all();
+            $builder->where(function ($query) use ($trackingProjects) {
+                foreach ($trackingProjects as $project) {
+                    $query->orWhere(function ($subQuery) use ($project) {
+                        $subQuery->orWhere('company_id', '=', $project->company_id);
+                        if (!is_null($project->team_id)) {
+                            $subQuery->whereHas('assignedToTeams', function ($q) use ($project) {
+                                $q->where('team_id', '=', $project->team_id);
+                            });
+                        }
+                    });
+                }
+            });
             return $builder;
         }
         return $builder;
