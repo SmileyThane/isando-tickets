d9600972813775c7d9ecbafc9c18a3af14a0afca
Hleb Prakhnitski <thane.crios@gmail.com>
2023-11-19 18:39:18 +0400

    fix

--- app/Http/Controllers/API/KbController.php
+++ app/Http/Controllers/API/KbController.php
@@ -15,6 +15,7 @@ use App\Repositories\KbRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Throwable;
 
 class KbController extends Controller
 {
