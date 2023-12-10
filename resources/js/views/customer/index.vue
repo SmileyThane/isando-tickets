0a312d6781b6ef93ed5e200da45e543dba505bd8
Hleb Prakhnitski <thane.crios@gmail.com>
2023-12-10 17:54:37 +0400

    customer view refactoring

--- resources/js/views/customer/index.vue
+++ resources/js/views/customer/index.vue
@@ -275,7 +275,12 @@ export default {
                     align: 'start',
                     value: 'id',
                 },
-                {text: `${this.$store.state.lang.lang_map.company.logo}`, value: 'logo', align: 'center', sortable: false},
+                {
+                    text: `${this.$store.state.lang.lang_map.company.logo}`,
+                    value: 'logo',
+                    align: 'center',
+                    sortable: false
+                },
                 {text: `${this.$store.state.lang.lang_map.main.name}`, value: 'name'},
                 {text: this.$store.state.lang.lang_map.main.email, value: 'email', sortable: false},
                 {text: this.$store.state.lang.lang_map.main.phone, value: 'phone', sortable: false},
