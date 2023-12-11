c2b8ea64db6909e761f53a0b0e295e40e665790d
Hleb Prakhnitski <thane.crios@gmail.com>
2023-12-11 23:06:37 +0400

    fix

--- app/Notifications/SendTicketReply.php
+++ app/Notifications/SendTicketReply.php
@@ -59,7 +59,7 @@ class SendTicketReply extends Notification implements ShouldQueue
             ->bcc(Config::get('mail.bcc.address'));
 
         foreach ($this->attachments as $attachment) {
-            $message->attachData(File::get(public_path('storage/' . $attachment->filepath . $attachment->name), $attachment->name));
+            $message->attach(File::get(public_path('storage/' . $attachment->filepath . $attachment->name)));
         }
 
         $message->view('ticket_reply', ['reply' => $this->message]);
