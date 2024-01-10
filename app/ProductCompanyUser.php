a6bb62b4f88557109b3b0ebf5be13a3c15eb0335
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-10 13:04:40 +0400

    added new product to employee relation

--- app/ProductCompanyUser.php
+++ app/ProductCompanyUser.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductCompanyUser extends Model
