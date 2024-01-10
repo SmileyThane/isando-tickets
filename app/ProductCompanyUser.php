6dda49234034499d4064f5bea4d77ff3f138189a
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-10 13:04:38 +0400

    added new product to employee relation

--- app/ProductCompanyUser.php
+++ app/ProductCompanyUser.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class ProductCompanyUser extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = ['product_id', 'company_user_id'];
+
+    public function productData(): HasOne
+    {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
 }
