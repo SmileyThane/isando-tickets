aa4c8d272709e7c8eb0367497455e65d797b4ec4
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-14 12:47:58 +0400

    pdf fix

--- resources/views/tracking/pdf/layout.blade.php
+++ resources/views/tracking/pdf/layout.blade.php
@@ -5,16 +5,16 @@
     <title></title>
 </head>
 <body>
-    <header class="header" style="border-left: 1px solid #000;">
+<header class="header" style="border-left: 1px solid #000;">
         <span class="header__name">
             {{ $reportName }}
         </span>
-        <br>
-        <span class="header__user">
+    <br>
+    <span class="header__user">
             <b>{{ $user->full_name }}</b>
         </span>
-        <br>
-        <span class="header__date">
+    <br>
+    <span class="header__date">
             {{ $period }}
         </span>
     </header>
