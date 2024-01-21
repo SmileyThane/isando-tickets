8c6bf867145875e872128bb2b2bc4a61f20a3b0f
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-21 12:49:17 +0400

    pdf fix

--- resources/views/tracking/pdf/title.blade.php
+++ resources/views/tracking/pdf/title.blade.php
@@ -6,19 +6,19 @@
     {{ $styles }}
 </head>
 <body>
-    <div class="page-1 title">
-        <div class="page-1__title text-center">
-            {{ $reportName }}
-        </div>
-        <div class="page-1__user text-center">
-            {{ $user->full_name }}
-        </div>
-        <div class="page-1__date text-center">
-            {{ $period }}
-        </div>
-        <br>
-        <br>
-        <div class="page-1__total-time text-center">
+<div class="page-1 title">
+    <div class="page-1__title text-center">
+        {{ $reportName }}
+    </div>
+    <div class="page-1__user text-center">
+        {{ $user->full_name }}
+    </div>
+    <div class="page-1__date text-center">
+        {{ $period }}
+    </div>
+    <br>
+    <br>
+    <div class="page-1__total-time text-center">
             <span class="total-time__title text-center">
                 Total time
                 <hr width="150">
