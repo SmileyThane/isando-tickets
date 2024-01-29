57816fe3a129d56fcc4f034ff399f472eca69a05
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-29 15:59:20 +0400

    created new migrations

---
+++ database/migrations/2024_01_20_140007_create_client_filter_group_has_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_filter_group_has_clients', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('group_id');
+            $table->bigInteger('client_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_filter_group_has_clients');
+    }
+};
