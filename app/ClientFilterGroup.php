8535ae53c458d07d51f29b8d743a0cf4027af3d7
Hleb Prakhnitski <thane.crios@gmail.com>
2024-01-29 22:59:28 +0400

    created children relation

--- app/ClientFilterGroup.php
+++ app/ClientFilterGroup.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ClientFilterGroup extends Model
 {
     use HasFactory;
 
     protected $fillable = ['company_id', 'name'];
+
+    public function children():HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id', 'id');
+    }
 }
