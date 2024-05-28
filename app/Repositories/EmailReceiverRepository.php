902b4a474387c4cb0b3a553678e18ddc144f6b76
Hleb Prakhnitski <thane.crios@gmail.com>
2024-05-28 11:01:40 +0400

    updated email parser

--- app/Repositories/EmailReceiverRepository.php
+++ app/Repositories/EmailReceiverRepository.php
@@ -78,7 +78,7 @@ class EmailReceiverRepository
             if ($userGlobal) {
                 Log::info('email from ' . $userGlobal->name);
                 try {
-                    $ticketSubject = trim(str_replace(["Re:", "Fwd:"], "", $rawSubject));
+                    $ticketSubject = trim(str_replace(["Re:", "Fwd:", "AW:", "Fw:", "FW:"], "", $rawSubject));
                     Log::info("subject is $ticketSubject");
                     $cachedCount = MailCache::where('message_key', $key)->count();
                     if ($cachedCount === 0) {
