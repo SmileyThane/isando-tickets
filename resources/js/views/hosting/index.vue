e7499a144583f49586e70baaf97cb0da665df2df
oladkins <un372036@gmail.com>
2024-09-21 00:22:34 +0300

    wip

--- resources/js/views/hosting/index.vue
+++ resources/js/views/hosting/index.vue
@@ -17,7 +17,7 @@
             <v-data-table
               :footer-props="footerProps"
               :headers="headers"
-              :items="customers"
+              :items="externalSources"
               :loading="loading"
               :loading-text="langMap.main.loading"
               :options.sync="options"
@@ -60,7 +60,7 @@
                   circle
                 >
                 </v-pagination>
-                  <button @click="createExternalSource">123</button>
+                <button @click="createExternalSource">123</button>
               </template>
             </v-data-table>
           </div>
@@ -70,129 +70,133 @@
   </v-container>
 </template>
 <script>
-
 export default {
-    props: {
-        type: {
-            type: Number,
-            required: true,
-            default: 1
-        }
+  props: {
+    type: {
+      type: Number,
+      required: true,
+      default: 1,
     },
-    data() {
-        return {
-            snackbar: false,
-            actionColor: '',
-            snackbarMessage: '',
-            themeFgColor: this.$store.state.themeFgColor,
-            themeBgColor: this.$store.state.themeBgColor,
-            totalCustomers: 0,
-            lastPage: 0,
-            loading: this.themeBgColor,
-            langMap: this.$store.state.lang.lang_map,
-            options: {
-                page: 1,
-                sortDesc: [false],
-                sortBy: ['name'],
-                itemsPerPage: localStorage.itemsPerPage ? parseInt(localStorage.itemsPerPage) : 10
-            },
-            footerProps: {
-                showFirstLastPage: true,
-                itemsPerPageOptions: [
-                    {
-                        'text': 10,
-                        'value': 10,
-                    },
-                    {
-                        'text': 20,
-                        'value': 20,
-                    },
-                    {
-                        'text': 50,
-                        'value': 50,
-                    },
-                    {
-                        'text': 100,
-                        'value': 100,
-                    },
-                    {
-                        'text': this.$store.state.lang.lang_map.sidebar.all,
-                        'value': 10000,
-                    }
-                ]
-            },
-            headers: [
-                {text: 'ID', align: 'end', value: 'id', sortable: false},
-                {text: this.$store.state.lang.lang_map.main.type, value: 'type', sortable: false},
-                {text: this.$store.state.lang.lang_map.main.name, value: 'name'},
-                {text: this.$store.state.lang.lang_map.main.email, value: 'email'},
-                {text: this.$store.state.lang.lang_map.main.phone, value: 'phone'},
-                {text: this.$store.state.lang.lang_map.main.city, value: 'city'},
-                {text: this.$store.state.lang.lang_map.main.country, value: 'country'},
-                {text: this.$store.state.lang.lang_map.main.status, value: 'is_active'}
-            ],
-            customersSearch: '',
-            externalSources: []
-        }
-    },
-    mounted() {
-        this.getExternalSourcesByType(this.type)
-    },
-    methods: {
-        getExternalSourcesByType(type_id){
-            this.loading = this.themeBgColor;
-            axios.get(`/api/external-sources`,
-            //     {
-            //     params: {
-            //         type_id: type_id
-            //     }
-            // }
-            ).then(response => {
-                if(response.success) {
-                    this.externalSources = response.data;
-                }
-            })
+  },
+  data() {
+    return {
+      snackbar: false,
+      actionColor: '',
+      snackbarMessage: '',
+      themeFgColor: this.$store.state.themeFgColor,
+      themeBgColor: this.$store.state.themeBgColor,
+      totalCustomers: 0,
+      lastPage: 0,
+      loading: this.themeBgColor,
+      langMap: this.$store.state.lang.lang_map,
+      options: {
+        page: 1,
+        sortDesc: [false],
+        sortBy: ['name'],
+        itemsPerPage: localStorage.itemsPerPage
+          ? parseInt(localStorage.itemsPerPage)
+          : 10,
+      },
+      footerProps: {
+        showFirstLastPage: true,
+        itemsPerPageOptions: [
+          {
+            text: 10,
+            value: 10,
+          },
+          {
+            text: 20,
+            value: 20,
+          },
+          {
+            text: 50,
+            value: 50,
+          },
+          {
+            text: 100,
+            value: 100,
+          },
+          {
+            text: this.$store.state.lang.lang_map.sidebar.all,
+            value: 10000,
+          },
+        ],
+      },
+      headers: [
+          { text: 'Name', value: 'name' },
+        { text: 'Domain', align: 'end', value: 'domain', sortable: false },
+        {
+          text: 'Domain_prefix',
+          value: 'Domain_prefix',
+          sortable: false,
         },
-        createExternalSource(){
-            axios.post(`/api/external-sources`, {
-                domain: 'com',
-                domainPrefix: 'https',
-                uri: 'google',
-                name: 'Google name',
-                authName: 'auth_name',
-                auth_method: 'post method',
-                auth_headers: 'some headers',
-                password: 'qwer1234',
-                billingPrice: 1000,
-                billingCurrency: 1,
-                billingType: 1,
-                isBillingAutoRenewal: false,
-                // last_billet_at: '20.09.24',
-                externalSourceTypeId: 1,
-                entityId: 1,
-                entityType: '1'
+        { text: 'Uri', value: 'uri' },
 
-                // 'domain' => 'required|string',
-                // 'domainPrefix' => 'required|string',
-                // 'uri' => 'nullable|string',
-                // 'name' => 'required|string',
-                // 'authName' => 'required|string',
-                // 'authHeaders' => 'nullable|string',
-                // 'password' => 'nullable|string',
-                // 'billingPrice' => 'required|int',
-                // 'billingCurrency' => 'required|int',
-                // 'billingType' => 'required|int',
-                // 'isBillingAutoRenewal' => 'bool|string',
-                // 'entityId' => ['nullable', 'int', new EntityExistsRule($this->input('entityType'))],
-                // 'entityType' => 'nullable|string',
-                // 'externalSourceTypeId' => 'id|exists:external_source_types,id',
-            })
+        { text: 'Auth name', value: 'auth_name' },
+        { text: 'Auth method', value: 'city' },
+        {
+          text: 'Password',
+          value: 'password',
         },
-        updateItemsCount(value) {
-            this.options.itemsPerPage = value;
-            localStorage.itemsPerPage = value;
-            this.options.page = 1;
-        }
-    }
-}
+        {
+          text: 'Billing price',
+          value: 'billing_price',
+        },
+          {
+          text: 'Billing price',
+          value: 'billing_price',
+        },
+      ],
+      customersSearch: '',
+      externalSources: [],
+    };
+  },
+  mounted() {
+    this.getExternalSourcesByType(this.type);
+  },
+  methods: {
+    getExternalSourcesByType(type_id) {
+      this.loading = this.themeBgColor;
+      axios
+        .get(
+          `/api/external-sources`
+          //     ,{
+          //     params: {
+          //         type_id: Number(type_id)
+          //     }
+          // }
+        )
+        .then((response) => {
+            this.loading = false;
+          if (response.success) {
+            this.externalSources = response.data;
+          }
+        });
+    },
+    createExternalSource() {
+      axios.post(`/api/external-sources`, {
+        domain: 'com',
+        domain_prefix: 'https',
+        uri: 'google',
+        name: 'Google name',
+        auth_name: 'auth_name',
+        auth_method: 'post method',
+        auth_headers: '{"some header key": "some header value"}',
+        password: 'qwer1234',
+        billing_price: 1000,
+        billing_currency: 1,
+        billing_type: 1,
+        is_billing_auto_renewal: false,
+        external_source_type_id: 1,
+        entity_id: 1,
+        entity_type: '1',
+      });
+    },
+    updateItemsCount(value) {
+      this.options.itemsPerPage = value;
+      localStorage.itemsPerPage = value;
+      this.options.page = 1;
+    },
+  },
+};
 </script>
