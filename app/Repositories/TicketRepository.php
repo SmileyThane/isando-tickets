8c60fdc938534ef0a1dd1d6c97ee204438360c5f
Hleb Prakhnitski <thane.crios@gmail.com>
2024-09-24 18:53:24 +0400

    updated ticket list query

--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -22,7 +22,6 @@ use App\UserNotificationStatus;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Throwable;
@@ -67,6 +66,12 @@ class TicketRepository
     public function all(Request $request)
     {
 //        DB::enableQueryLog();
+
+//        $key = 'ticket_' . base64_encode(json_encode($request->all()));
+//        if (Cache::has($key)) {
+//            return Cache::get($key);
+//        }
+
         $ticketIds = [];
         $companyUser = Auth::user()->employee;
         $tickets = Ticket::query();
@@ -227,18 +232,15 @@ class TicketRepository
         } else {
             $ticketResult = $ticketResult->orderBy($orderedField, $orderedDirection);
         }
+
+        $result = $ticketResult->paginate($request->per_page ?? count($ticketIds));
+//        Cache::remember($key, 120, static function () use ($result) {
+//            return $result;
+//        });
+
 //        dd(DB::getQueryLog());
 
-        $key = 'ticket_' . base64_encode(json_encode($request->all()));
-        if (Cache::has($key)) {
-            return Cache::get($key);
-        }
-
-        Cache::remember($key, 600, static function () use ($ticketResult, $request, $ticketIds) {
-            return $ticketResult->paginate($request->per_page ?? count($ticketIds));
-        });
-
-        return $ticketResult->paginate($request->per_page ?? count($ticketIds));
+        return $result;
     }
 
     private function ticketAccessFilter($companyUser, $tickets)
