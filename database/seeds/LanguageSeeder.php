b26b52573f3654b4f16808102eee78cacbb5f0bf
oladkins <un372036@gmail.com>
2024-09-26 14:41:18 +0300

    add form validation, clear unused fields

--- database/seeds/LanguageSeeder.php
+++ database/seeds/LanguageSeeder.php
@@ -1118,7 +1118,9 @@ class LanguageSeeder extends Seeder
                          'save' => 'Save',
                          'close' => 'Close',
                          'delete_confirmation' => 'Are you sure you want to delete this source?',
-                         'id' => 'ID'
+                         'id' => 'ID',
+                         'payment' => 'Payment',
+                         'subscription' => 'Subscription'
                     ],
                 ])
             ]
@@ -2202,7 +2204,9 @@ class LanguageSeeder extends Seeder
                             'save' => 'Speichern',
                             'close' => 'Schließen',
                             'delete_confirmation' => 'Sind Sie sicher, dass Sie diese Quelle löschen möchten?',
-                            'id' => 'ID'
+                            'id' => 'ID',
+                            'payment' => 'Zahlung',
+                            'subscription' => 'Abonnement'
                         ],
                     ]
                 )
